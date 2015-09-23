de90e54af711bcad9009a8d7f132240929ff7b98
Tobias Lundell <toblu611@student.liu.se>
2015-09-23 10:40:56 +0200

    Added graphical Hello World for LOVE

--- Project/src/main.lua
+++ Project/src/main.lua
@@ -1,6 +1,10 @@
+
+
 local function main()
-
-  print 'Hello World'
-
+  print 'Hello World!'
+--    function love.draw() -- This function is for drawing "Hello World! in the graphical window when running LÖVE. Uncomment when running with LÖVE
+--      love.graphics.print("Hello World!", 400, 300)
+--    end
 end
+
 main()
