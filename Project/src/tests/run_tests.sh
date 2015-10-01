ee7a9d2ca02b8f7686a09b23fb1965c5364093da
jonathan <jonathan@jonathananderson.se>
2015-10-01 09:45:28 +0200

    added  return of exitcode 1 if test failed

--- Project/src/tests/run_tests.sh
+++ Project/src/tests/run_tests.sh
@@ -2,6 +2,11 @@
 export LUA_PATH="/usr/local/share/lua/5.2/?.lua;/usr/local/share/lua/5.2/?/init.lua;/usr/local/lib/lua/5.2/?.lua;/usr/local/lib/lua/5.2/?/init.lua;/usr/share/lua/5.2/?.lua;/usr/share/lua/5.2/?/init.lua;./?.lua;./?/init.lua;./tests/lemock/?.lua;./tests/luacov/src/?.lua;./tests/lunit/?.lua"
 export LUA_CPATH="/usr/local/lib/lua/5.2/?.so;/usr/lib/x86_64-linux-gnu/lua/5.2/?.so;/usr/lib/lua/5.2/?.so;/usr/local/lib/lua/5.2/loadall.so;./?.so"
 tests/lunit/lunit tests/init.lua
+EXITCODE=$?
+if [ $EXITCODE -ne 0 ]
+  then
+    exit 1
+fi
 STAT_FILE="$(ls tests/luacov/luacov.report.* | sort -n -t _ -k 2 -r | head -1)"
 OUTPUT=$STAT_FILE'.xml'
 lua tests/luacov/src/bin/luacov-cobertura -o "tests/luacov/coverage.xml"
