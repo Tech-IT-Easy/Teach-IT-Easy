fbd56487aa3454d8288a7f0235dd6899f672331a
Erik Jansson <erija860@student.liu.se>
2015-10-08 11:51:20 +0200

    Added images. Changed some code structure.

--- Project/Emulator/main.lua
+++ Project/Emulator/main.lua
@@ -2,7 +2,7 @@ ADConfig = require("Config.ADConfig")
 ADLogger = require("SDK.Utils.ADLogger")
 ADLogger.trace("Applicatio Init")
 
-if ADConfig.isSimulator then
+--[[if ADConfig.isSimulator then
 
   gfx = require "SDK.Simulator.gfx"
   zto = require "SDK.Simulator.zto"
@@ -11,10 +11,28 @@ if ADConfig.isSimulator then
   freetype = require "SDK.Simulator.freetype"
   sys = require "SDK.Simulator.sys"
 end
+--]]
 
-local button_inactive = gfx.loadpng('data/activeitem.png')
+if ADConfig.isSimulator then
+
+  gfx = require "SDK.Simulator.gfx"
+  zto = require "SDK.Simulator.zto"
+  surface = require "SDK.Simulator.surface"
+  player = require "SDK.Simulator.player"
+  freetype = require "SDK.Simulator.freetype"
+  sys = require "SDK.Simulator.sys"
+  script_path = ""
+else
+  script_path = sys.root_path()
+end
+
 
 local background = gfx.loadpng('data/background.png')
+local image1 = gfx.loadpng('data/bowser.png')
+local image2 = gfx.loadpng('data/mario.png')
+local image3 = gfx.loadpng('data/toad.png')
+local images ={image1, image2, image3}
+
 local pos = 1;
 
 --Interface scaling variables
@@ -33,20 +51,19 @@ local addprofileheight = screen:get_height()*0.16
 local activeaddprofileheight = addprofileheight * 0.96
 local activeaddprofiley = addprofiley+(addprofileheight-activeaddprofileheight)
 
---Fonts
-local appname = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.04, {x= screen:get_width()*0.43, y=appnamebaseline}, 'data/BlackoutMidnight.ttf')
-local pagename = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.07, {x= screen:get_width()*0.24, y=pagenamebaseline}, 'data/Chalkduster.ttf')
-local addprofileplus = sys.new_freetype({g=131, r=0, b=143}, screen:get_height()*0.075, {x= screen:get_width()*0.35, y=screen:get_height()*0.883}, 'data/BlackoutMidnight.ttf')
-local addprofilename = sys.new_freetype({g=131, r=0, b=143}, screen:get_height()*0.05, {x= screen:get_width()*0.38, y=screen:get_height()*0.88}, 'data/Chalkduster.ttf')
+-- Fonts
+-- Needs to be changed to absolute path on STB, or implement solution
+local appname = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.04, {x= screen:get_width()*0.43, y=appnamebaseline}, script_path..'data/BlackoutMidnight.ttf')
+local pagename = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.07, {x= screen:get_width()*0.24, y=pagenamebaseline}, script_path..'data/Chalkduster.ttf')
+local addprofileplus = sys.new_freetype({g=131, r=0, b=143}, screen:get_height()*0.075, {x= screen:get_width()*0.35, y=screen:get_height()*0.883}, script_path..'data/BlackoutMidnight.ttf')
+local addprofilename = sys.new_freetype({g=131, r=0, b=143}, screen:get_height()*0.05, {x= screen:get_width()*0.38, y=screen:get_height()*0.88}, script_path..'data/Chalkduster.ttf')
 
 function onKey(key,state)
   ADLogger.trace("OnKey("..key..","..state..")")
-  if key == 'right' and state == 'down' and pos < 4 then pos = pos + 1 end
-  if key == 'left' and state == 'down' and pos > 1 then pos = pos - 1 end
-  if key == 'down' and state == 'down' then pos = 5 end
-  if key == 'up' and state == 'down' and pos == 5 then pos = 1 end
-  print(pos)
-  renderUI()
+  if key == 'right' and state == 'down' and pos < 4 then inactive(pos) pos = pos + 1 active(pos) end
+  if key == 'left' and state == 'down' and pos > 1 then inactive(pos) pos = pos - 1 active(pos) end
+  if key == 'down' and state == 'down' then inactive(pos) pos = 5 active(pos) end
+  if key == 'up' and state == 'down' and pos == 5 then inactive(pos) pos = 1 active(pos) end
   gfx.update()
 end
 
@@ -71,69 +88,38 @@ function onStart()
 end
 
 function active(x1)
-  if x1==1 then
-    screen:fill({g=131, r=0, b=143}, {x=hspacing, y=itemy, w=itemwidth, h= itemheight})
-    screen:fill({g=255, r=255, b=255}, {x=hspacing + itemwidth*0.02 , y=activey, w=activewidth, h=activeheight})
-  end
-
-  if x1==2 then
-    screen:fill({g=131, r=0, b=143}, {x=(hspacing*2)+itemwidth, y=itemy, w=itemwidth, h= itemheight})
-    screen:fill({g=255, r=255, b=255}, {x=(hspacing*2)+itemwidth + itemwidth*0.02 , y=activey, w=activewidth, h=activeheight})
-  end
-
-  if x1==3 then
-    screen:fill({g=131, r=0, b=143}, {x=(hspacing*3)+(itemwidth*2), y=itemy, w=itemwidth, h= itemheight})
-    screen:fill({g=255, r=255, b=255}, {x=(hspacing*3)+(itemwidth*2) + itemwidth*0.02 , y=activey, w=activewidth, h=activeheight})
-  end
-
-  if x1==4 then
-    screen:fill({g=131, r=0, b=143}, {x=(hspacing*4)+(itemwidth*3), y=itemy, w=itemwidth, h= itemheight})
-    screen:fill({g=255, r=255, b=255}, {x=(hspacing*4)+(itemwidth*3) + itemwidth*0.02 , y=activey, w=activewidth, h=activeheight})
+  if x1<5 then
+    screen:clear({g=131, r=0, b=143}, {x=(hspacing*x1)+ itemwidth*(x1-1), y=itemy, w=itemwidth, h= itemheight})
+    screen:clear({g=255, r=255, b=255}, {x=(hspacing*x1)+ itemwidth*(x1-1)+ itemwidth*0.02 , y=activey, w=activewidth, h=activeheight})
+    if x1<(table.getn(images)+1) then
+      --images[x1]:set_alpha(255)
+      screen:copyfrom(images[x1], nil, {x=(hspacing*x1)+ itemwidth*(x1-1)+screen:get_width()*0.025,y=itemy+screen:get_height()*0.01,w=image1:get_width()*0.6, h=image1:get_height()*0.6},true)
+    end
+  else
+    screen:clear({g=131, r=0, b=143}, {x=0, y=addprofiley, w=screen:get_width(), h=addprofileheight})
+    screen:clear({g=255, r=255, b=255}, {x=0, y=activeaddprofiley, w=screen:get_width(), h=activeaddprofileheight})
+    addprofileplus:draw_over_surface(screen, "+")
+    addprofilename:draw_over_surface(screen, "ADD A PROFILE")
   end
 end
 
 function inactive(x1)
-  --screen:fill({g=228, r=187, b=235}, {x=x1, y=itemy, w=itemwidth, h=itemheight})
-
-  if x1==1 then
-    screen:fill({g=228, r=187, b=235}, {x=hspacing, y=itemy, w=itemwidth, h= itemheight})
-  end
-
-  if x1==2 then
-    screen:fill({g=228, r=187, b=235}, {x=(hspacing*2)+itemwidth, y=itemy, w=itemwidth, h= itemheight})
-  end
-
-  if x1==3 then
-    screen:fill({g=228, r=187, b=235}, {x=(hspacing*3)+(itemwidth*2), y=itemy, w=itemwidth, h= itemheight})
-  end
-
-  if x1==4 then
-    screen:fill({g=228, r=187, b=235}, {x=(hspacing*4)+(itemwidth*3), y=itemy, w=itemwidth, h= itemheight})
+  if x1<5 then
+    screen:clear({g=228, r=187, b=235}, {x=(hspacing*x1)+ itemwidth*(x1-1), y=itemy, w=itemwidth, h= itemheight})
+    if x1<(table.getn(images)+1) then
+    --images[x1]:set_alpha(150)
+      screen:copyfrom(images[x1], nil, {x=(hspacing*x1)+ itemwidth*(x1-1)+screen:get_width()*0.025,y=itemy+screen:get_height()*0.01,w=image1:get_width()*0.6, h=image1:get_height()*0.6},true)
+    end
+  else
+    screen:clear({g=228, r=187, b=235}, {x=0, y=addprofiley, w=screen:get_width(), h=addprofileheight})
+    addprofileplus:draw_over_surface(screen, "+")
+    addprofilename:draw_over_surface(screen, "ADD A PROFILE")
   end
 end
 
 function renderUI()
-
-  if pos==1 then active(1)
-  else inactive(1) end
-
-  if pos==5 then screen:fill({g=131, r=0, b=143}, {x=0, y=addprofiley, w=screen:get_width(), h=addprofileheight})
-    screen:fill({g=255, r=255, b=255}, {x=0, y=activeaddprofiley, w=screen:get_width(), h=activeaddprofileheight})
-    addprofileplus:draw_over_surface(screen, "+")
-    addprofilename:draw_over_surface(screen, "ADD A PROFILE")
-  else screen:fill({g=228, r=187, b=235}, {x=0, y=addprofiley, w=screen:get_width(), h=addprofileheight}) 
-  addprofileplus:draw_over_surface(screen, "+")
-  addprofilename:draw_over_surface(screen, "ADD A PROFILE")
+  active(1)
+  for i=2, 5, 1 do
+    inactive(i)
   end
-
-
-  if pos==2 then active(2)
-  else inactive(2) end
-
-  if pos==3 then active(3)
-  else inactive(3) end
-
-  if pos==4 then active(4)
-  else inactive(4) end
-
 end
