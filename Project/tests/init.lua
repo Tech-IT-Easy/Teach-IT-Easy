de14ab08798c23fdc64217ea8cf75193130524bd
Tobias Lundell <toblu611@student.liu.se>
2015-10-10 19:19:57 +0200

    Copied the code from jonathan2 into Emulator and removed jonathan2 folder

--- Project/tests/init.lua
+++ Project/tests/init.lua
@@ -2,7 +2,7 @@ require "luacov"
 require "tests.mock_emulator"
 -- Put your files after this line:
 
-require "tests.stb_int_tests"
-require "tests.Emulator"
+--require "tests.stb_int_tests"
+--require "tests.Emulator"
 -- Examlpe:
 require "tests.my_folder"
