70ebc524e035cb8ce634805e3943830f37051683
jonathan <jonathan@jonathananderson.se>
2015-10-12 11:53:46 +0200

    DON'T DELETE THE TEST FOLDER!!!

---
+++ Project/tests/run_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export LUA_PATH="/usr/local/share/lua/5.2/?.lua;/usr/local/share/lua/5.2/?/init.lua;/usr/local/lib/lua/5.2/?.lua;/usr/local/lib/lua/5.2/?/init.lua;/usr/share/lua/5.2/?.lua;/usr/share/lua/5.2/?/init.lua;./?.lua;./?/init.lua;./tests/lemock/?.lua;./tests/luacov/src/?.lua;./tests/lunit/?.lua;./Emulator/?.lua;../?.lua;../?/init.lua;../tests/lemock/?.lua;../tests/luacov/src/?.lua;../tests/lunit/?.lua;"
+export LUA_CPATH="/usr/local/lib/lua/5.2/?.so;/usr/lib/x86_64-linux-gnu/lua/5.2/?.so;/usr/lib/lua/5.2/?.so;/usr/local/lib/lua/5.2/loadall.so;./?.so"
+cd Emulator
+../tests/lunit/lunit ../tests/init.lua
+EXITCODE=$?
+STAT_FILE="$(ls ../tests/luacov/luacov.report.* | sort -n -t _ -k 2 -r | head -1)"
+OUTPUT=$STAT_FILE'.xml'
+lua ../tests/luacov/src/bin/luacov-cobertura -o "../tests/luacov/coverage.xml"
+rm ../tests/luacov/luacov.stats.out
+cd ..
+exit $EXITCODE
