f6b3a8f821254af9e7c77f346d1a675018a3db32
Erik Jansson <erija860@student.liu.se>
2015-10-13 15:20:10 +0200

    Added functionality in MainMenu and Games.

--- Project/Emulator/classes/Games.lua
+++ Project/Emulator/classes/Games.lua
@@ -14,7 +14,7 @@ function Games:new()
   -- fonts
   -- Needs to be changed to absolute path on STB, or implement solution
   self.appname = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.04, {x= screen:get_width()*0.43, y=self.appnamebaseline}, script_path..'data/BlackoutMidnight.ttf')
-  self.pagename = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.07, {x= screen:get_width()*0.375, y=self.pagenamebaseline}, script_path..'data/Chalkduster.ttf')
+  self.pagename = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.07, {x= screen:get_width()*0.33, y=self.pagenamebaseline}, script_path..'data/Chalkduster.ttf')
   self.username = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.035, {x= screen:get_width()*0.15, y=self.usernamebaseline}, script_path..'data/BlackoutMidnight.ttf')
 
   self.pos = 1
@@ -51,6 +51,22 @@ function Games:renderui()
   self.pagename:draw_over_surface(screen, "CHOOSE A GAME")
   self.username:draw_over_surface(screen, self.usernamestring)
   screen:clear({g=228, r=187, b=235}, {x=screen:get_width()*0.08, y=screen:get_height()*0.09, w=screen:get_width()*0.06, h= screen:get_height()*0.1})
+  self:buttonactive(1)
+end
+
+function Games:buttonactive(x)
+  screen:clear({ g = 255, r = 255, b = 255 }, { x = screen:get_width()*0.1 + (screen:get_width()*0.2)*(x-1) , y = (screen:get_height()*0.25) , w = screen:get_width()*0.18, h = screen:get_height()*0.45 })
+
+  --local sidebuttontext = sys.new_freetype({ g = 131, r = 0, b = 143}, screen:get_height() * 0.07, { x = screen:get_width()*0.65, y = (screen:get_height()*0.35) + ((screen:get_height()*0.2125)*(x-1)) }, script_path .. 'data/condensed.ttf')
+  --sidebuttontext:draw_over_surface(screen, self.sidebuttons[x])
+end
+
+function Games:buttoninactive(x)
+  screen:clear({ g = 228, r = 187, b = 235 }, { x = screen:get_width()*0.5375, y = (screen:get_height()*0.3) + ((screen:get_height()*0.2125)*(x-1)) , w = screen:get_width()*0.425, h = screen:get_height()*0.185 })
+  screen:clear({ g = 228, r = 187, b = 235 }, { x = screen:get_width()*0.1 + (screen:get_width()*0.2)*(x-1) , y = (screen:get_height()*0.25) , w = screen:get_width()*0.18, h = screen:get_height()*0.45 })
+  
+  --local sidebuttontext = sys.new_freetype({ g = 131, r = 0, b = 143}, screen:get_height() * 0.07, { x = screen:get_width()*0.65, y = (screen:get_height()*0.35) + ((screen:get_height()*0.2125)*(x-1)) }, script_path .. 'data/condensed.ttf')
+  --sidebuttontext:draw_over_surface(screen, self.sidebuttons[x])
 end
 
 return Games
--- Project/Emulator/classes/MainMenu.lua
+++ Project/Emulator/classes/MainMenu.lua
@@ -20,22 +20,44 @@ function MainMenu:new()
   self.appname = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.04, {x= screen:get_width()*0.43, y=self.appnamebaseline}, script_path..'data/BlackoutMidnight.ttf')
   self.pagename = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.07, {x= screen:get_width()*0.375, y=self.pagenamebaseline}, script_path..'data/Chalkduster.ttf')
   self.username = sys.new_freetype({g=255, r=255, b=255}, screen:get_height()*0.035, {x= screen:get_width()*0.15, y=self.usernamebaseline}, script_path..'data/BlackoutMidnight.ttf')
-  self.games = sys.new_freetype({g=131, r=0, b=143}, screen:get_height()*0.1, {x= screen:get_width()*0.19, y=(screen:get_height()*0.65)}, script_path..'data/BlackoutMidnight.ttf')
+  self.games = sys.new_freetype({g=131, r=0, b=143}, screen:get_height()*0.1, {x= screen:get_width()*0.165, y=(screen:get_height()*0.75)}, script_path..'data/BlackoutMidnight.ttf')
 
-  self.pos = 1
+  self.sidebuttons = {"TROPHY ROOM", "WARDROBE", "SETTINGS"}
+
   return self
 end
 
 
 function MainMenu:handleinput(key)
 
-  if key == '1' then return {"games", self.usernamestring} else return {" "} end
+  if key == 'right' and self.pos ==0 then
+    self:gamebuttoninactive()
+    self.pos = 1
+    self:sidebuttonactive(self.pos)
+  end
+  if key == 'down' and self.pos >0 and self.pos < 3 then
+    self:sidebuttoninactive(self.pos)
+    self.pos = self.pos + 1
+    self:sidebuttonactive(self.pos)
+  end
+  if key == 'up' and self.pos > 1 and self.pos < 4 then
+    self:sidebuttoninactive(self.pos)
+    self.pos = self.pos -1
+    self:sidebuttonactive(self.pos)
+  end
+  if key == 'left' and self.pos > 0 and self.pos < 4 then
+    self:sidebuttoninactive(self.pos)
+    self.pos = 0
+    self:gamebuttonactive()
+  end
+  if key == '1' and self.pos == 0 then return {"games", self.usernamestring} else return {" "} end
 
 end
 
 
 
 function MainMenu:loadview(input)
+  self.pos = 0
   self.usernamestring = input
   self:printbackground()
 
@@ -55,17 +77,35 @@ function MainMenu:renderui()
   self.username:draw_over_surface(screen, self.usernamestring)
   screen:clear({g=228, r=187, b=235}, {x=screen:get_width()*0.08, y=screen:get_height()*0.09, w=screen:get_width()*0.06, h= screen:get_height()*0.1})
   self:gamebuttonactive()
+  for i = 1, 3, 1 do
+    self:sidebuttoninactive(i)
+  end
+
 end
 
 function MainMenu:gamebuttonactive()
-  screen:clear({g=131, r=0, b=143}, {x=screen:get_width()*0.1, y=screen:get_height()*0.3, w=screen:get_width()*0.35, h= screen:get_height()*0.5})
-  screen:clear({g=255, r=255, b=255}, {x=screen:get_width()*0.105 , y=(screen:get_height()*0.309), w=(screen:get_width()*0.34), h=(screen:get_height()*0.483)})
+  screen:clear({g=131, r=0, b=143}, {x=screen:get_width()*0.05, y=screen:get_height()*0.3, w=screen:get_width()*0.42, h= screen:get_height()*0.61})
+  screen:clear({g=255, r=255, b=255}, {x=screen:get_width()*0.055 , y=(screen:get_height()*0.309), w=(screen:get_width()*0.41), h=(screen:get_height()*0.593)})
   self.games:draw_over_surface(screen, "GAMES")
 end
 
 function MainMenu:gamebuttoninactive()
-  screen:clear({g=228, r=187, b=235}, {x=screen:get_width()*0.1, y=screen:get_height()*0.3, w=screen:get_width()*0.35, h= screen:get_height()*0.5})
+  screen:clear({g=228, r=187, b=235}, {x=screen:get_width()*0.05, y=screen:get_height()*0.3, w=screen:get_width()*0.42, h= screen:get_height()*0.61})
   self.games:draw_over_surface(screen, "GAMES")
+end
+
+function MainMenu:sidebuttonactive(x)
+  screen:clear({ g = 131, r = 0, b = 143 }, { x = screen:get_width()*0.5375, y = (screen:get_height()*0.3) + ((screen:get_height()*0.2125)*(x-1)) , w = screen:get_width()*0.425, h = screen:get_height()*0.185 })
+  screen:clear({g=255, r=255, b=255}, {x=screen:get_width()*0.5425 , y=(screen:get_height()*0.309)+ ((screen:get_height()*0.2125)*(x-1)), w=(screen:get_width()*0.415), h=(screen:get_height()*0.1685)})
+
+  local sidebuttontext = sys.new_freetype({ g = 131, r = 0, b = 143}, screen:get_height() * 0.07, { x = screen:get_width()*0.65, y = (screen:get_height()*0.35) + ((screen:get_height()*0.2125)*(x-1)) }, script_path .. 'data/condensed.ttf')
+  sidebuttontext:draw_over_surface(screen, self.sidebuttons[x])
+end
+
+function MainMenu:sidebuttoninactive(x)
+  screen:clear({ g = 228, r = 187, b = 235 }, { x = screen:get_width()*0.5375, y = (screen:get_height()*0.3) + ((screen:get_height()*0.2125)*(x-1)) , w = screen:get_width()*0.425, h = screen:get_height()*0.185 })
+  local sidebuttontext = sys.new_freetype({ g = 131, r = 0, b = 143}, screen:get_height() * 0.07, { x = screen:get_width()*0.65, y = (screen:get_height()*0.35) + ((screen:get_height()*0.2125)*(x-1)) }, script_path .. 'data/condensed.ttf')
+  sidebuttontext:draw_over_surface(screen, self.sidebuttons[x])
 end
 
 
