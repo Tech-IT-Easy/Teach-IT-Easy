22fe6369776b874ca1118d0f2f549f8bfb5e4a9d
Johanna Skolling <johsk926@student.liu.se>
2015-10-13 15:04:23 +0200

    Added test to ProfileSelection. One test fails although it shouldn't, don't know why but am working on it. One test is auto-fail since the testing of this class isn't complete and one test fails because of incorrect functionality.

--- Project/tests/Emulator/classes/test_ProfileSelection.lua
+++ Project/tests/Emulator/classes/test_ProfileSelection.lua
@@ -43,6 +43,8 @@ end
 
 function setup()
   a = my_file:new()
+  a.usernames = {"a", "b", "c"}
+
 end
 
 function teardown()
@@ -56,14 +58,90 @@ function test_loadview()
 end
 
 
-function test_handleinput()
-  -- make things here
-  a.usernames = {"a", "b"}
+function test_handleinput_right()
+  -- goes right from pos 1
   a.pos = 1
   a:handleinput('right')
-  assert_equal(2, a.pos, "shouldn't be 2")
+  assert_equal(2, a.pos, "should move one step right, didn't")
+end
+
+function test_handleinput_right_two()
+   -- does not move right when at far right
+   a.pos = 3
+   a:handleinput('right')
+   assert_equal(3, a.pos, "moved right even though it was positioned far right")
+end
+
+function test_handleinput_left()
+    -- does not move left when at far left pos
+    a.pos = 1
+    a:handleinput('left')
+    assert_equal(1, a.pos, "tried to move left even though is was posistioned far left")
+end
+
+function test_handleinput_left_two()
+   a.pos = 3
+   a.handleinput('left')
+   assert_equal(2, a.pos, "did not move left, should have.")
+end
+
+function test_handleinput_bottom_right()
+  -- does not move from bottom when right is pressed
+  a.pos = 5
+  a:handleinput('right')
+  assert_equal(5, a.pos, "moved from bottom when right was pressed")
+end
+
+function test_handleinput_bottom_left()
+  -- does not move from bottom when left is pressed
+  a.pos = 5
+  a:handleinput('left')
+  assert_equal(5, a.pos, "moved from bottom when left was pressed")
+end
+
+function test_handleinput_bottom_down()
+ -- does not move from bottom when down is pressed
+  a.pos = 5
+  a:handleinput('down')
+  assert_equal(5, a.pos, "moved from bottom when down was pressed")
+end
+
+function test_handleinput_bottom_up()
+  -- moves up from bottom when up is pressed
+  a. pos = 5
+  a:handleinput('up')
+  assert_equal(1, a.pos, "did not move up from bottom when up was pressed")
+end
+
+function test_handleinput_up_up()
+  -- nothing should happen when up is pressed and pos is among the upper ones.
+  a.pos = 1
+  a:handleinput('up')
+  assert_equal(1, a.pos, "tried to move from pos 1 when it was already up")
+  a.pos = 2
+  a:handleinput('up')
+  assert_equal(2, a.pos, "tried to move from pos 2 when it was already up")
+  a.pos = 3
+  a:handleinput('up')
+  assert_equal(3, a.pos, "tried to move from pos 3 when it was already up")
 
 end
+
+function test_handleinput_up_down()
+    -- should move down to pos 5 from all upper pos when down is pressed a.pos = 1
+  a.pos = 1
+  a:handleinput('down')
+  assert_equal(5, a.pos, "should have moved down from pos 1 to pos 5")
+  a.pos = 2
+  a:handleinput('down')
+  assert_equal(5, a.pos, "should have moved down from pos 2 to pos 5")
+  a.pos = 3
+  a:handleinput('down')
+  assert_equal(5, a.pos, "should have moved down from pos 3 to pos 5")
+
+end
+
+
 function test_printbackground()
   a:printbackground()
 end
