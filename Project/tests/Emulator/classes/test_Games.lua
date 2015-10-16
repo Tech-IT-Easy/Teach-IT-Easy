4bc355127926749b7297f1e764338f0056cc2d62
Johanna Skolling <johsk926@student.liu.se>
2015-10-16 08:28:24 +0200

    Added failing tests for CreateProfile and Games

---
+++ Project/tests/Emulator/classes/test_Games.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: johannaskolling
+-- Date: 15-10-16
+-- Time: 08:27
+-- To change this template use File | Settings | File Templates.
+--
+
+
+lunit = require "lunit"
+module( "Emulator_classes_Games", package.seeall, lunit.testcase )
+my_file = require "Emulator.classes.Games"
+
+function test_MainView_and_fail()
+   fail("Games not tested, always fail")
+end
+
