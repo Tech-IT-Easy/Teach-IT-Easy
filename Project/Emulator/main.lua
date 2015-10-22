340216f36a56a93f6d327fab43092454990c31d9
Erik Jansson <erija860@student.liu.se>
2015-10-22 11:56:04 +0200

    testing on stb

--- Project/Emulator/main.lua
+++ Project/Emulator/main.lua
@@ -58,10 +58,10 @@ end
 -------------------------------------
 function loadviews()
      profileselection = ProfileSelection:new()
-    mainmenu = MainMenu:new()
-    createprofile = CreateProfile:new()
-    games = Games:new()
-    views = { profilesel = profileselection, main = mainmenu, create = createprofile, games = games }
+    --mainmenu = MainMenu:new()
+    --createprofile = CreateProfile:new()
+    --games = Games:new()
+    views = { profilesel = profileselection}--, main = mainmenu, create = createprofile, games = games }
     currentview = "profilesel"
 end
 
