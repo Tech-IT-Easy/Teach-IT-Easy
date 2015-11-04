466a9d05a5ae88ac54513b69dda7f17be436837c
Tobias Lundell <toblu611@student.liu.se>
2015-11-04 20:20:05 +0100

    Started implementing ChooseAvatar menu. Now possible to create a new profile, but does not yet show avatars.

--- Project/src/menus/ChooseAvatar.lua
+++ Project/src/menus/ChooseAvatar.lua
@@ -9,6 +9,7 @@
 ChooseAvatar = {} --MenuView:new()
 
 local Event = require('toolkit.Event')
+local CreateProfile = require('menus.CreateProfile')
 
 function ChooseAvatar:new()
 
@@ -17,6 +18,19 @@ end
 
 function ChooseAvatar:update()
 
+end
+
+function ChooseAvatar:handleinput(event)
+  if (event.key == Event.KEY_ONE) then
+    table.insert(usernames, CreateProfile.profilename)
+    self.image4 = gfx.loadpng('data/bowser.png')
+    table.insert(images, self.image4)
+    return { "profilesel", " " }
+  end
+  if (event.key == Event.KEY_TWO) then
+    CreateProfile.profilename = ""
+    return { "create" }
+  end
 end
 
 function ChooseAvatar:loadview(input)
@@ -31,7 +45,7 @@ end
 function ChooseAvatar:renderui()
   create_prof_appname:draw_over_surface(screen, "TEACH IT EASY")
   create_prof_pagename:draw_over_surface(screen, "CHOOSE YOUR AVATAR")
-  --screen:clear({ g = 255, r = 255, b = 255 }, { x = screen:get_width() * 0.1, y = screen:get_height() * 0.25 * 1.2, w = screen:get_width() * 0.8, h = screen:get_height() * 0.3 * 0.6 })
+ -- screen:clear({ g = 255, r = 255, b = 255 }, { x = screen:get_width() * 0.1, y = screen:get_height() * 0.25 * 1.2, w = screen:get_width() * 0.8, h = screen:get_height() * 0.3 * 0.6 })
  -- screen:clear({ g = 131, r = 0, b = 143 }, { x = 0, y = screen:get_height() * 0.55, w = screen:get_width(), h = screen:get_height() * 0.45 })
 
 end
