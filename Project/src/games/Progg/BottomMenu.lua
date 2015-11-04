da4592dd705c5632a8a8525137f3ff888c1a6b5d
Mikael <mikog745@student.liu.se>
2015-11-04 11:44:05 +0100

    Created basic structure for classes BottomMenu and RightMenu

---
+++ Project/src/games/Progg/BottomMenu.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------
+-- This class keeps track of the images and position of the
+-- commands in the bottom menu based on how they're
+-- organized in the queue.
+--
+-- @Author:Created by Mikael Ögren, Nov 04,2015
+-- @Author:Updated by author,date
+-----------------------------------------------------------
+
+local Object = require("toolkit.Object")
+
+local BottomMenu = extends(Object.class())
+
+function BottomMenu:rearrage()
+
+
+end
+
+return BottomMenu
---
+++ Project/src/games/Progg/RightMenu.lua
@@ -0,0 +1,19 @@
+-----------------------------------------------------------
+-- This class keeps track of the images and the position
+-- of the marker to properly display the right menu.
+--
+-- @Author:Created by Mikael Ögren, Nov 04,2015
+-- @Author:Updated by author,date
+-----------------------------------------------------------
+
+
+local Object = require("toolkit.Object")
+
+local RightMenu = extends(Object.class())
+
+function RightMenu:highlight()
+
+
+end
+
+return RightMenu
