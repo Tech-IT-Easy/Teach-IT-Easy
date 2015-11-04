7b026416d2f27b5cb595f3bc70cc492ccff205c6
Mario <Mario@Equipo-M>
2015-11-04 11:39:44 +0100

    Added input handler basic structure class. Modified Event class adding all numbers

---
+++ Project/src/games/Progg/GameInputHandler.lua
@@ -0,0 +1,66 @@
+ADConfig = require("Config.ADConfig")
+ADLogger = require("SDK.Utils.ADLogger")
+ADLogger.trace("Application Init")
+
+if ADConfig.isSimulator then
+  gfx = require "SDK.Simulator.gfx"
+  zto = require "SDK.Simulator.zto"
+  surface = require "SDK.Simulator.surface"
+  player = require "SDK.Simulator.player"
+  freetype = require "SDK.Simulator.freetype"
+  sys = require "SDK.Simulator.sys"
+end
+
+local Controllable = require("toolkit.Controllable")
+local EventHandler = require('toolkit.EventHandler')
+local Event = require('toolkit.Event')
+
+GameInputHandler = extends(Controllable.class())
+
+
+-----------------------------------------------------------
+-- Construct method 
+-- -------------------------------------------------------
+function GameInputHandler:new()
+
+end
+
+function GameInputHandler:load()
+
+end
+
+--Subscribing the eventHandler to all events. Only numbers by now
+exampleEventHandler = EventHandler:new()
+exampleEventHandler.events = {[Event.KEY_ONE] = 1,[Event.KEY_TWO] = 1,[Event.KEY_THREE]=1,[Event.KEY_FOUR]=1,[Event.KEY_FIVE]=1,[Event.KEY_SIX]=1,[Event.KEY_SEVEN]=1,[Event.KEY_EIGHT]=1,[Event.KEY_NINE]=1,[Event.KEY_ZERO]=1}
+
+--Update function on every key input
+function exampleEventHandler:update(object,eventListener,event)
+
+  --Switch for all the input handling to implement
+  if event.key == Event.KEY_ONE then
+ 
+  elseif event.key == Event.KEY_TWO then
+  
+  elseif event.key == Event.KEY_THREE then
+  
+  elseif event.key == Event.KEY_FOUR then
+  
+  elseif event.key == Event.KEY_FIVE then
+  
+  elseif event.key == Event.KEY_SIX then
+   
+  elseif event.key == Event.KEY_SEVEN then
+    
+  elseif event.key == Event.KEY_EIGHT then
+     
+  elseif event.key == Event.KEY_NINE then
+      
+  elseif event.key == Event.KEY_ZERO then
+ 
+ end
+ return true
+end
+
+GameInputHandler.eventHandler = exampleEventHandler
+
+return GameInputHandler
