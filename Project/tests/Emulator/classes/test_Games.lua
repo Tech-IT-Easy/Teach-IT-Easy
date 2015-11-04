50cb44fc7a360e66d6b227c42827d470c84d4d89
Johanna Skolling <johsk926@student.liu.se>
2015-11-04 11:21:44 +0100

    Fixed test_Games so that is works with new architecture

--- Project/tests/Emulator/classes/test_Games.lua
+++ Project/tests/Emulator/classes/test_Games.lua
@@ -14,7 +14,7 @@ local event = require "src.toolkit.Event"
 
 
 -- System under test
-local SUT = 'Emulator.classes.Games'
+local SUT = 'src.menus.Games'
 local function create_mock(class_to_mock)
   -- unload the package if loaded to dissmiss previous mocks
   package.loaded[class_to_mock] = nil
@@ -74,11 +74,38 @@ function test_handleinput_right_from_left()
   a:handleinput(event)
   assert_equal(2, a.pos, "should move one position to the right, didn't ")
 end
---[[
+
 
 -- tests if move right from far right position, shouldn't
 function test_handleinput_right_from_right()
- fail("don't know how to test if key == 'right' and self.pos <#self.games then..., Don't know how to test #self.games")
+       local mc = create_mock(SUT)
+       -- Mock inactive and active
+       -- Create mock objects for each function to mock
+
+       local buttoninactive = mc:mock()
+       local buttonactive = mc:mock()
+
+       -- In this case we want to mock 2 member functions so then we import the SUT (System under test)
+       local ps = require(SUT)
+
+       -- ocerride the original functions with mocks
+       package.loaded[SUT].buttoninactive = buttoninactive
+       package.loaded[SUT].buttonactive = buttonactive
+
+       -- Tell for which arguments it should work, what it should return and how many times it should be called.
+       buttoninactive(mc.ANYARGS) ;mc :returns(nil) :anytimes()
+       buttonactive(mc.ANYARGS) ;mc :returns(nil) :anytimes()
+
+       -- start the testing
+       mc:replay()
+
+       local a = ps:new()
+       a.pos = 4
+       event.key = event.KEY_RIGHT
+       a:handleinput(event)
+       assert_equal(4, a.pos, "should not move right!")
+
+ --fail("don't know how to test if key == 'right' and self.pos <#self.games then..., Don't know how to test #self.games")
 end
 
 -- tests if move left from position that is not the far left one
@@ -105,7 +132,8 @@ function handleinput_left_form_right()
 
   local a = ps:new()
   a.pos = 3
-  a:handleinput('left')
+  event.key= event.KEY_LEFT
+  a:handleinput(event)
   assert_equal(2, a.pos, "should move one position to the left, didn't ")
 end
 
@@ -133,7 +161,8 @@ function test_handleinput_left_form_left()
 
   local a = ps:new()
   a.pos = 1
-  a:handleinput('left')
+  event.key = event.KEY_LEFT
+  a:handleinput(event)
   assert_equal(1, a.pos, "should note move one position to the left, did ")
 end
 
@@ -142,7 +171,8 @@ function test_handleinput_two_from_left()
   local ps = require(SUT)
   local a = ps:new()
   a.pos = 1
-  local b = a:handleinput('2')
+  event.key = event.KEY_TWO
+  local b = a:handleinput(event)
   assert_equal("main", b[1], "should return main")
 end
 -- checks if main is return when trying to go back.
@@ -150,7 +180,8 @@ function test_handleinput_other_args()
   local ps = require(SUT)
   local a = ps:new()
   a.pos = 2
-  local b = a:handleinput('h')
+  event.key = 'h'
+  local b = a:handleinput(event)
   assert_equal(" ", b[1], "should return nothing and thereby not want to do anything")
 end
 
@@ -160,4 +191,3 @@ end
 function test_Games_and_fail()
    fail("Games not completely tested, always fail")
 end
-]]
