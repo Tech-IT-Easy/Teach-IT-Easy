94351177785cd57b1ce4994215332e954323bdf0
A.M <andma767@student.liu.se>
2015-11-05 11:59:41 +0100

    Added unit tests for classes in Progg folder.

---
+++ Project/tests/Emulator/test_programminggame/test_BottomMenu.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Andreas Mansson
+-- Date: 2015-11-05
+-- Time: 11:38
+-- To change this template use File | Settings | File Templates.
+--
+
+lunit = require "lunit"
+module( "test_BottomMenu", package.seeall, lunit.testcase )
+
+local SUT = 'src.games.Progg.BottomMenu'
+
+-- Delete this function when the real test is done
+function test_BottomMenu_fail()
+   fail("BottomMenu class and functions not tested yet!")
+end
---
+++ Project/tests/Emulator/test_programminggame/test_Position.lua
@@ -0,0 +1,17 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Andreas Mansson
+-- Date: 2015-11-05
+-- Time: 11:49
+-- To change this template use File | Settings | File Templates.
+--
+
+lunit = require "lunit"
+module( "test_Position", package.seeall, lunit.testcase )
+
+local SUT = 'src.games.Progg.Position'
+
+-- Delete this function when the real test is done
+function test_Position_fail()
+   fail("Position class and functions not tested yet!")
+end
