56e2c1bfa82a2184f16d25cb0fd4f3ced748d223
Erik Jansson <erija860@student.liu.se>
2015-11-05 13:38:28 +0100

    Added pic of programming game when its pressed.

--- Project/src/menus/Games.lua
+++ Project/src/menus/Games.lua
@@ -35,6 +35,8 @@ function Games:handleinput(event)
 ----------------------------------------------------
 --  Launches game and discards the menu to save space
 ----------------------------------------------------
+    proggImage = gfx.loadpng('data/ProgrammingGamePic.jpg')
+    screen:copyfrom(proggImage, nil, { x = 0, y = 0, w = screen:get_width(), h = screen:get_height() }, true)
     PlatformContext.game = GameFactory:getGame(self.games[self.pos][1],PlatformContext)
     PlatformContext.game:start()
     PlatformContext.platformMenu = nil
