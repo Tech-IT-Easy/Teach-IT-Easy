a9239b6d7a34cd3837efaef07a0d9b283bd9e4d0
Andreas Mansson <andma767@student.liu.se>
2015-11-06 09:01:52 +0100

    Changed test structure and updated search paths in files.

--- Project/tests/run_tests.sh
+++ Project/tests/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export LUA_PATH="/usr/local/share/lua/5.2/?.lua;/usr/local/share/lua/5.2/?/init.lua;/usr/local/lib/lua/5.2/?.lua;/usr/local/lib/lua/5.2/?/init.lua;/usr/share/lua/5.2/?.lua;/usr/share/lua/5.2/?/init.lua;./?.lua;./?/init.lua;./tests/lemock/?.lua;./tests/luacov/src/?.lua;./tests/lunit/?.lua;./Emulator/?.lua;./src/?.lua;../?.lua;../?/init.lua;../tests/lemock/?.lua;../tests/luacov/src/?.lua;../tests/lunit/?.lua;"
+export LUA_PATH="/usr/local/share/lua/5.2/?.lua;/usr/local/share/lua/5.2/?/init.lua;/usr/local/lib/lua/5.2/?.lua;/usr/local/lib/lua/5.2/?/init.lua;/usr/share/lua/5.2/?.lua;/usr/share/lua/5.2/?/init.lua;./?.lua;./?/init.lua;./tests/lemock/?.lua;./tests/luacov/src/?.lua;./tests/lunit/?.lua;./src/?.lua;./src/?.lua;../?.lua;../?/init.lua;../tests/lemock/?.lua;../tests/luacov/src/?.lua;../tests/lunit/?.lua;"
 export LUA_CPATH="/usr/local/lib/lua/5.2/?.so;/usr/lib/x86_64-linux-gnu/lua/5.2/?.so;/usr/lib/lua/5.2/?.so;/usr/local/lib/lua/5.2/loadall.so;./?.so"
 cd src
 ../tests/lunit/lunit ../tests/init.lua
