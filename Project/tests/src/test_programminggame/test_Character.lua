843e5fb3028231bfea5f191c643383cfabf287bb
Mario <Mario@Equipo-M>
2015-11-11 10:45:07 +0100

    Tests for character

--- Project/tests/src/test_programminggame/test_Character.lua
+++ Project/tests/src/test_programminggame/test_Character.lua
@@ -15,3 +15,51 @@ local SUT = 'src.games.Progg.Character'
 function test_Character_fail()
    fail("Character class and functions not tested yet!")
 end
+
+
+-- Testing position of the character creation and state
+function test_Character_new()
+
+    local a = require(SUT)
+    local char = a:new(2,1)
+    local x = 2
+    local y = 1
+    local stt = 0
+    
+    lunit.assert_not_equal(char.position:getX(),x,"The character is not created on the right position X")
+    lunit.assert_not_equal(char.position:getY(),y,"The character is not created on the right position Y")
+    lunit.assert_not_equal(char.state,stt,"The character is not created on the right position Y")
+end
+
+--Test for execute a command move when state down
+function test_Character_move()
+
+    local a = require(SUT)
+    local Command = require('src.games.Progg.Commands')
+    local char = a:new(0,0)
+    char:execute(Command.MOVE)
+    
+    lunit.assert_not_equal(char.position:getY(),-5,"The character doesnt move forward down propperly")
+end
+
+--Test for execute a command turn-left
+function test_Character_turnLeft()
+
+    local a = require(SUT)
+    local Command = require('src.games.Progg.Commands')
+    local char = a:new(0,0)
+    char:execute(Command.TURN_LEFT)
+    
+    lunit.assert_not_equal(char.state,3,"The character doesnt change state propperly when turned left")
+end
+
+--Test for execute a command turn-right
+function test_Character_turnRight()
+
+    local a = require(SUT)
+    local Command = require('src.games.Progg.Commands')
+    local char = a:new(0,0)
+    char:execute(Command.TURN_RIGHT)
+    
+    lunit.assert_not_equal(char.state,1,"The character doesnt change state propperly when turned right")
+end
