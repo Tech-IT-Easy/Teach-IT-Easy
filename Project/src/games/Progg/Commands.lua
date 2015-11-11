e759a4def7a7f839c064a09d30186a5a4757e757
Vilhelm Granath <vilhelm.granath@gmail.com>
2015-11-11 10:50:16 +0100

    Changing so that functions are called with command rather that number

--- Project/src/games/Progg/Commands.lua
+++ Project/src/games/Progg/Commands.lua
@@ -7,15 +7,13 @@ local Object = require("toolkit.Object")
 
 local Commands = extends(Object.class())
 
-Commands.KEY_ONE = "move"
-Commands.KEY_TWO = "turn-c"
-Commands.KEY_THREE = "turn-cc"
-Commands.KEY_FOUR = "action"
-Commands.KEY_FIVE = "if"
-Commands.KEY_SIX = "loop"
-Commands.KEY_SEVEN = "p1"
-Commands.KEY_EIGHT = "p2"
-Commands.KEY_NINE = "placeholder"
+Commands.MOVE = "move"
+Commands.TURN_LEFT = "turn-left"
+Commands.TURN_RIGHT = "turn-right"
+Commands.LOOP_START = "loop-start"
+Commands.LOOP_END = "loop-end"
+Commands.IF_END = "if-end"
+
 
 -------------------------------------
 --Constructor to create a new command-object.
--- Project/src/games/Progg/RightMenu.lua
+++ Project/src/games/Progg/RightMenu.lua
@@ -3,7 +3,7 @@
 -- of the marker to properly display the right menu.
 --
 -- @Author:Created by Mikael Ögren, Nov 04,2015
--- @Author:Updated by Vilhelm Granath,Nov 10, 2015
+-- @Author:Updated by Vilhelm Granath, Nov 10, 2015
 -----------------------------------------------------------
 
 local Object = require("toolkit.Object")
@@ -13,17 +13,21 @@ skin = require('games/Progg/progg_skin')
 -- Variale to keep track of a highlighted command
 highlight = nill
 
+-- Available commands
+commands = {"move","turn-left","turn-right","action","if-wall","loop","P1","P2"}
+
 -- images
-img1 = gfx.loadpng('data/progg_game_icons/arrow_up.png')
-img2 = gfx.loadpng('data/progg_game_icons/turn_left.png')
-img3 = gfx.loadpng('data/progg_game_icons/turn_right.png')
-img4 = gfx.loadpng('data/progg_game_icons/action.png')
-img5 = gfx.loadpng('data/progg_game_icons/if_wall.png')
-img6 = gfx.loadpng('data/progg_game_icons/loop.png')
-img7 = gfx.loadpng('data/progg_game_icons/P1.png')
-img8 = gfx.loadpng('data/progg_game_icons/P2.png')
-img9 = gfx.loadpng('data/progg_game_icons/P2.png')
-images = { img1, img2, img3, img4, img5, img6, img7, img8, img9 }
+move = gfx.loadpng('data/progg_game_icons/arrow_up.png')
+turnLeft = gfx.loadpng('data/progg_game_icons/turn_left.png')
+turnRight = gfx.loadpng('data/progg_game_icons/turn_right.png')
+action = gfx.loadpng('data/progg_game_icons/action.png')
+ifWall = gfx.loadpng('data/progg_game_icons/if_wall.png')
+loop = gfx.loadpng('data/progg_game_icons/loop.png')
+p1 = gfx.loadpng('data/progg_game_icons/P1.png')
+p2 = gfx.loadpng('data/progg_game_icons/P2.png')
+local images = { ["move"]=move, ["turn-left"]=turnLeft, ["turn-right"]=turnRight, ["action"]=action,
+    ["if-wall"]=ifWall, ["loop"]=loop, ["P1"]=p1, ["P2"]=p2 }
+
 
 -------------------------------------
 -- Creates the Right-hand screen in the programming game
@@ -38,7 +42,39 @@ function RightMenu:new()
     self:drawRow(3)
     self:drawFullRow(4,78,113,215)
     self:addNumbers()
+    self:addImages()
+    self:highlight("action")
+    self:highlight("P2")
+    self:play()
+    -- self:loop()
     return self.class()
+end
+
+-------------------------------------
+-- Assigns commands to numbers
+--
+-- @params command: name of the action
+-- @returns corresponding position in the grid
+-- @author Vilhelm
+-------------------------------------
+function RightMenu:getPosition(command)
+    if command == "move" then
+        return 1
+    elseif command == "turn-left" then
+        return 2
+    elseif command == "turn-right" then
+        return 3
+    elseif command == "action" then
+        return 4
+    elseif command == "if-wall" then
+        return 5
+    elseif command == "loop" then
+        return 6
+    elseif command == "P1" then
+        return 7
+    elseif command == "P2" then
+        return 8
+    end
 end
 
 -------------------------------------
@@ -46,14 +82,35 @@ end
 -- @author Vilhelm
 -------------------------------------
 function RightMenu:drawRow(row)
-     self:drawBox(34,59,94,203,212,214,first_column,first_row+(row-1)*(command_height+col_spacing),command_width,command_height)
-     screen:copyfrom(images[(row-1)*3+1], nil, { x = first_column+6, y = first_row+(row-1)*(command_height+col_spacing)+6, w=command_width-12, h = command_height-12 }, true)
-     
-     self:drawBox(34,59,94,203,212,214,first_column+(command_width+row_spacing),first_row+(row-1)*(command_height+col_spacing),command_width,command_height)
-     screen:copyfrom(images[(row-1)*3+2], nil, { x = first_column+(command_width+row_spacing)+6, y = first_row+(row-1)*(command_height+col_spacing)+6, w=command_width-12, h = command_height-12 }, true)
-     
-     self:drawBox(34,59,94,203,212,214,first_column+2*(command_width+row_spacing),first_row+(row-1)*(command_height+col_spacing),command_width,command_height)
-     screen:copyfrom(images[(row-1)*3+3], nil, { x = first_column+2*(command_width+row_spacing)+6, y = first_row+(row-1)*(command_height+col_spacing)+6, w=command_width-12, h = command_height-12 }, true)
+    self:drawBox(34,59,94,203,212,214,first_column,first_row+(row-1)*(command_height+col_spacing),command_width,command_height)
+    self:drawBox(34,59,94,203,212,214,first_column+(command_width+row_spacing),first_row+(row-1)*(command_height+col_spacing),command_width,command_height)
+    self:drawBox(34,59,94,203,212,214,first_column+2*(command_width+row_spacing),first_row+(row-1)*(command_height+col_spacing),command_width,command_height)
+end
+
+-------------------------------------
+-- Adds images for all commands
+-- to the 9-command layout
+-- @author Vilhelm
+-------------------------------------
+function RightMenu:addImages()
+    local i = 1
+    while commands[i] do
+        print(commands[i])
+        self:addImage(commands[i])
+        i = i + 1
+    end
+end
+
+-------------------------------------
+-- Adds a images for a specified
+-- command in the 9-command layout
+-- @author Vilhelm
+-------------------------------------
+function RightMenu:addImage(command)
+    local position=self:getPosition(command)
+    local xvalue = first_column+((position-1)%3)*(command_width+row_spacing)+6
+    local yvalue = first_row+math.floor((position-1)/3)*(command_height+col_spacing)+6
+    screen:copyfrom(images[command], nil, { x = xvalue, y = yvalue, w=command_width-12, h = command_height-12 }, true)
 end
 
 -------------------------------------
@@ -61,16 +118,16 @@ end
 -- @author Vilhelm
 -------------------------------------
 function RightMenu:addNumbers()
-     command_1:draw_over_surface(screen, "1")
-     command_2:draw_over_surface(screen, "2")
-     command_3:draw_over_surface(screen, "3")
-     command_4:draw_over_surface(screen, "4")
-     command_5:draw_over_surface(screen, "5")
-     command_6:draw_over_surface(screen, "6")
-     command_7:draw_over_surface(screen, "7")
-     command_8:draw_over_surface(screen, "8")
-     command_9:draw_over_surface(screen, "9")
-     command_play:draw_over_surface(screen, "0  Play!")
+    command_1:draw_over_surface(screen, "1")
+    command_2:draw_over_surface(screen, "2")
+    command_3:draw_over_surface(screen, "3")
+    command_4:draw_over_surface(screen, "4")
+    command_5:draw_over_surface(screen, "5")
+    command_6:draw_over_surface(screen, "6")
+    command_7:draw_over_surface(screen, "7")
+    command_8:draw_over_surface(screen, "8")
+    command_9:draw_over_surface(screen, "9")
+    command_play:draw_over_surface(screen, "0  Play!")
 end
 
 
@@ -79,25 +136,25 @@ end
 -- @author Vilhelm
 -------------------------------------
 function RightMenu:addSingleNumber(position)
-   if position == 1 then
-     command_1:draw_over_surface(screen, "1")
-   elseif position == 2 then
-     command_2:draw_over_surface(screen, "2")
-   elseif position == 3 then
-     command_3:draw_over_surface(screen, "3")
-   elseif position == 4 then
-     command_4:draw_over_surface(screen, "4")
-   elseif position == 5 then
-     command_5:draw_over_surface(screen, "5")
-   elseif position == 6 then
-     command_6:draw_over_surface(screen, "6")
-   elseif position == 7 then
-     command_7:draw_over_surface(screen, "7")
-   elseif position == 8 then
-     command_8:draw_over_surface(screen, "8")
-   elseif position == 9 then
-     command_9:draw_over_surface(screen, "9")
-   end
+    if position == 1 then
+        command_1:draw_over_surface(screen, "1")
+    elseif position == 2 then
+        command_2:draw_over_surface(screen, "2")
+    elseif position == 3 then
+        command_3:draw_over_surface(screen, "3")
+    elseif position == 4 then
+        command_4:draw_over_surface(screen, "4")
+    elseif position == 5 then
+        command_5:draw_over_surface(screen, "5")
+    elseif position == 6 then
+        command_6:draw_over_surface(screen, "6")
+    elseif position == 7 then
+        command_7:draw_over_surface(screen, "7")
+    elseif position == 8 then
+        command_8:draw_over_surface(screen, "8")
+    elseif position == 9 then
+        command_9:draw_over_surface(screen, "9")
+    end
 end
 
 -------------------------------------
@@ -117,46 +174,47 @@ end
 -------------------------------------
 function RightMenu:drawBox(r1,g1,b1,r2,g2,b2,x,y,w,h)
 
-    if w>h
+    --[[if w>h
     then
-      f=h
+      local f=h
     else
-      f=w
-    end
-    
-    b=3
-    x2=x+b
-    y2=y+b
-    h2=h-2*b
-    w2=w-2*b
-      
+      local f=w
+    end]]
+
+    local b=3
+    local x2=x+b
+    local y2=y+b
+    local h2=h-2*b
+    local w2=w-2*b
+
     screen:clear({ r = r1, g = g1, b = b1 }, { x = x, y = y, w = w, h = h})
     screen:clear({ r = r2, g = g2, b = b2 }, { x = x2, y = y2, w = w2, h = h2})
-     
+
 end
 
 -------------------------------------
 -- Highlights the command in the
 -- specified position.
--- 
+--
 -- Needs to implement a timed call to remove 
 -- the highlight again
--- 
+--
 -- @author Vilhelm
 -------------------------------------
-function RightMenu:highlight(position)
-  if highlight ~= nill then
-    self:removeHighlight(highlight)
-  end
-  
-  xvalue = first_column+((position-1)%3)*(command_width+row_spacing)
-  yvalue = first_row+math.floor((position-1)/3)*(command_height+col_spacing)
-  
-  self:drawBox(255,192,0,203,212,214,xvalue,yvalue,command_width,command_height)
-  screen:copyfrom(images[position], nil, { x = xvalue+6, y = yvalue+6, w=command_width-12, h = command_height-12 }, true)
-  self:addSingleNumber(position)
-  
-  highlight = position
+function RightMenu:highlight(command)
+    if highlight ~= nill then
+        self:removeHighlight(highlight)
+    end
+
+    local position = self:getPosition(command)
+    local xvalue = first_column+((position-1)%3)*(command_width+row_spacing)
+    local yvalue = first_row+math.floor((position-1)/3)*(command_height+col_spacing)
+
+    self:drawBox(255,192,0,203,212,214,xvalue,yvalue,command_width,command_height)
+    screen:copyfrom(images[command], nil, { x = xvalue+6, y = yvalue+6, w=command_width-12, h = command_height-12 }, true)
+    self:addSingleNumber(position)
+
+    highlight = command
 
 end
 
@@ -165,33 +223,34 @@ end
 -- specified position. 
 -- @author Vilhelm
 -------------------------------------
-function RightMenu:removeHighlight(position)
+function RightMenu:removeHighlight(command)
+    local position = self:getPosition(command)
 
-  xvalue = first_column+((position-1)%3)*(command_width+row_spacing)
-  yvalue = first_row+math.floor((position-1)/3)*(command_height+col_spacing)
-  
-  self:drawBox(34,59,94,203,212,214,xvalue,yvalue,command_width,command_height)
-  screen:copyfrom(images[position], nil, { x = xvalue+6, y = yvalue+6, w=command_width-12, h = command_height-12 }, true)
-  self:addSingleNumber(position)
-  
-  highlight = nill
-  
+    local xvalue = first_column+((position-1)%3)*(command_width+row_spacing)
+    local yvalue = first_row+math.floor((position-1)/3)*(command_height+col_spacing)
+
+    self:drawBox(34,59,94,203,212,214,xvalue,yvalue,command_width,command_height)
+    screen:copyfrom(images[command], nil, { x = xvalue+6, y = yvalue+6, w=command_width-12, h = command_height-12 }, true)
+    self:addSingleNumber(position)
+
+    highlight = nill
+
 end
 
 -------------------------------------
 -- Changes the play-button to a stop-button when 
 -- the play-button is pressed
 -- Also removes any highlight on the commands
--- 
+--
 -- Needs to stop the timer if there is one
--- 
+--
 -- @author Vilhelm
 -------------------------------------
 function RightMenu:play()
     if highlight ~= nill then
-      self:removeHighlight(highlight)
+        self:removeHighlight(highlight)
     end
-    
+
     self:drawFullRow(4,245,45,120)
     command_play:draw_over_surface(screen, "0  Stop!")
 end
@@ -206,20 +265,23 @@ function RightMenu:stop()
     command_play:draw_over_surface(screen, "0  Play!")
 end
 
-
+-------------------------------------
+-- Called to replace buttons for loops
+-- @author Vilhelm
+-------------------------------------
 function RightMenu:loop()
+    -- draw rows
+    -- add numbers in boxes
     self:drawFullRow(4,78,113,215)
-    
+    -- image for loop symbol
 end
-
-
 
 -------------------------------------
 -- I'm unsure what this function was 
 -- meant to do /Vilhelm
--- 
+--
 -- Used to load images
--- @author 
+-- @author
 -------------------------------------
 function RightMenu:load()
 
