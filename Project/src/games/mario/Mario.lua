1f3ceadf73ed189cf6f29eb593e019ca7afa1cd0
Kai Chu <kaich284@student.liu.se>
2015-11-12 14:33:28 +0100

    merge test hotfix1

--- Project/src/games/mario/Mario.lua
+++ Project/src/games/mario/Mario.lua
@@ -61,7 +61,7 @@ end
 function Mario:load()
   -- load resources 
   self.exampleActor:load()
-  print("load resouces1")
+  print("load resouces2")
 end
 
 -----------------------------------------------------------
