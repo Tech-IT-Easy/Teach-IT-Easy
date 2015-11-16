5e50f477c3111a1944b56bb21e74a81775a46012
Mikael <mikog745@student.liu.se>
2015-11-16 11:23:40 +0100

    Moved draw functions from BottomMenu to new class DrawBottomMenu

--- Project/src/games/Progg/BottomMenu.lua
+++ Project/src/games/Progg/BottomMenu.lua
@@ -11,11 +11,14 @@
 local Object = require("toolkit.Object")
 local BottomMenu = extends(Object)
 
+local newDrawBottomMenu = require("games.Progg.DrawBottomMenu")
+
 --Constructor method
 function BottomMenu:new(maxCommands)
     local o = BottomMenu:super()
     o.availableSlots = maxCommands
     o.queue = {}
+    o.drawBottomMenu = newDrawBottomMenu:new()
     return BottomMenu:init(o)
 end
 
@@ -35,9 +38,9 @@ end
 
 --Used when BottomMenu is updated
 function BottomMenu:show(inputArea)
-    self:drawBackground(inputArea)
-    self:drawEmptySlots(self.availableSlots)
-    self:drawIcons(self.queue)
+    self.drawBottomMenu:Background(inputArea)
+    self.drawBottomMenu:EmptySlots(self.availableSlots)
+    self.drawBottomMenu:Icons(self.queue, self.images)
 end
 
 -------------------------------------
@@ -49,61 +52,6 @@ function BottomMenu:setQueue(queue)
     self.queue = queue
 end
 
--------------------------------------
--- Draw all empty command slots for current level.
--- @param maxCommands. How many commands slots that are available to the player.
--- @author Mikael Ögren
--------------------------------------
-function BottomMenu:drawEmptySlots(maxCommands)
-    for i=1, maxCommands do
-            self:drawSingleEmptySlot(i)
-    end
-end
-
--------------------------------------
--- Draws a single empty command slot
--- @param boxNmb. The number of the box being drawn.
--- @author Mikael Ögren
--------------------------------------
-function BottomMenu:drawSingleEmptySlot(boxNmb)
-    if boxNmb <= 8 then
-        screen:clear({r = 78, g = 113, b = 215 }, { x = screen:get_width()*(0.035 + (boxNmb-1)*0.055), y = screen:get_height()*0.74, w = screen:get_width()*0.045, h = screen:get_height()*0.075 }) --r = 78, g = 113, b = 215
-    else
-        screen:clear({r = 78, g = 113, b = 215 }, { x = screen:get_width()*(0.035 + (boxNmb-9)*0.055), y = screen:get_height()*0.84, w = screen:get_width()*0.045, h = screen:get_height()*0.075 })
-    end
-end
-
--------------------------------------
--- Draws the background for the bottom left menu
--- @author Mikael Ögren
--------------------------------------
-function BottomMenu:drawBackground(inputArea)
-    screen:clear({r = 27, g = 39, b = 53 }, { x = 0, y = screen:get_height() * 0.65, w = screen:get_width(), h = screen:get_height() * 0.35 })
-    if (inputArea == "queue") then
-        screen:clear({r = 209, g = 209, b = 209 }, { x = screen:get_width() * 0.53, y = screen:get_height() * 0.6999, w = screen:get_width() * 0.44, h = screen:get_height() * 0.25 })
-    else
-        screen:clear({r = 35, g = 73, b = 120 }, { x = screen:get_width() * 0.53, y = screen:get_height() * 0.6999, w = screen:get_width() * 0.44, h = screen:get_height() * 0.25 })
-    end
-end
-
-
--------------------------------------
--- Draws the icons for the bottom left menu.
--- @param queue. The queue of commands. An array of strings.
--- @author Mikael Ögren
--------------------------------------
-function BottomMenu:drawIcons(queue)
-    for i = 1, #queue do
-        if i <= 8 then
-            print(queue[i])
-            screen:clear({r = 255, g = 255, b = 251 }, { x = screen:get_width()*(0.038 + (i-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
-            screen:copyfrom(self.images[queue[i]], nil, { x = screen:get_width()*(0.038 + (i-1)*0.055), y = screen:get_height()*0.744, w=screen:get_width()*0.038, h = screen:get_height()*0.066 }, true)
-        else
-            screen:clear({r = 255, g = 255, b = 251 }, { x = screen:get_width()*(0.038 + (i-9)*0.055), y = screen:get_height()*0.8435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
-            screen:copyfrom(self.images[queue[i]], nil, { x = screen:get_width()*(0.038 + (i-9)*0.055), y = screen:get_height()*0.844, w=screen:get_width()*0.038, h = screen:get_height()*0.066 }, true)
-        end
-    end
-end
 
 
 return BottomMenu
---
+++ Project/src/games/Progg/DrawBottomMenu.lua
@@ -0,0 +1,78 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Mikael
+-- Date: 11/16/2015
+-- Time: 11:08 AM
+-- To change this template use File | Settings | File Templates.
+--
+
+local Object = require("toolkit.Object")
+local DrawBottomMenu = extends(Object)
+
+function DrawBottomMenu:new()
+    local o = DrawBottomMenu:super()
+    o.queue = {}
+    return DrawBottomMenu:init(o)
+end
+
+-------------------------------------
+-- Draw all empty command slots for current level.
+-- @param maxCommands. How many commands slots that are available to the player.
+-- @author Mikael Ögren
+-------------------------------------
+function DrawBottomMenu:EmptySlots(maxCommands)
+    for i=1, maxCommands do
+            self:SingleEmptySlot(i)
+    end
+end
+
+-------------------------------------
+-- Draws a single empty command slot
+-- @param boxNmb. The number of the box being drawn.
+-- @author Mikael Ögren
+-------------------------------------
+function DrawBottomMenu:SingleEmptySlot(boxNmb)
+    if boxNmb <= 8 then
+        screen:clear({r = 78, g = 113, b = 215 }, { x = screen:get_width()*(0.035 + (boxNmb-1)*0.055), y = screen:get_height()*0.74, w = screen:get_width()*0.045, h = screen:get_height()*0.075 }) --r = 78, g = 113, b = 215
+    else
+        screen:clear({r = 78, g = 113, b = 215 }, { x = screen:get_width()*(0.035 + (boxNmb-9)*0.055), y = screen:get_height()*0.84, w = screen:get_width()*0.045, h = screen:get_height()*0.075 })
+    end
+end
+
+-------------------------------------
+-- Draws the background for the bottom left menu
+-- @author Mikael Ögren
+-------------------------------------
+function DrawBottomMenu:Background(inputArea)
+    screen:clear({r = 27, g = 39, b = 53 }, { x = 0, y = screen:get_height() * 0.65, w = screen:get_width(), h = screen:get_height() * 0.35 })
+    if (inputArea == "queue") then
+        screen:clear({r = 209, g = 209, b = 209 }, { x = screen:get_width() * 0.53, y = screen:get_height() * 0.6999, w = screen:get_width() * 0.44, h = screen:get_height() * 0.25 })
+    else
+        screen:clear({r = 35, g = 73, b = 120 }, { x = screen:get_width() * 0.53, y = screen:get_height() * 0.6999, w = screen:get_width() * 0.44, h = screen:get_height() * 0.25 })
+    end
+end
+
+
+-------------------------------------
+-- Draws the icons for the bottom left menu.
+-- @param queue. The queue of commands. An array of strings.
+-- @author Mikael Ögren
+-------------------------------------
+function DrawBottomMenu:Icons(queue, images)
+    for i = 1, #queue do
+        if i <= 8 then
+            print(queue[i])
+            screen:clear({r = 255, g = 255, b = 251 }, { x = screen:get_width()*(0.038 + (i-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
+            screen:copyfrom(images[queue[i]], nil, { x = screen:get_width()*(0.038 + (i-1)*0.055), y = screen:get_height()*0.744, w=screen:get_width()*0.038, h = screen:get_height()*0.066 }, true)
+        else
+            screen:clear({r = 255, g = 255, b = 251 }, { x = screen:get_width()*(0.038 + (i-9)*0.055), y = screen:get_height()*0.8435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
+            screen:copyfrom(images[queue[i]], nil, { x = screen:get_width()*(0.038 + (i-9)*0.055), y = screen:get_height()*0.844, w=screen:get_width()*0.038, h = screen:get_height()*0.066 }, true)
+        end
+    end
+end
+
+
+
+
+return DrawBottomMenu
+
