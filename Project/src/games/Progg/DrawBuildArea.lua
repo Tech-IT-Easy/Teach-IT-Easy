89c94ec715d33f9162dff732abf45b9c37d453bf
Tobias Lundell <toblu611@student.liu.se>
2015-11-19 20:24:23 +0100

    Testing transparency with screen:fill

--- Project/src/games/Progg/DrawBuildArea.lua
+++ Project/src/games/Progg/DrawBuildArea.lua
@@ -112,19 +112,19 @@ function DrawBuildArea:highlightIcon(position,prevPos, queue)
     if queue[self.pos] ~= nil then
         self.image = gfx.loadpng(self:getFileName(queue[self.pos]))
         if self.pos <= 8 then
-            screen:clear({r = 0, g = 0, b = 0 }, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
+            screen:fill({r = 0, g = 0, b = 0, a = 20 }, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
             screen:copyfrom(self.image, nil, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.744, w=screen:get_width()*0.038, h = screen:get_height()*0.066 }, true)
         else
-            screen:clear({r = 0, g = 0, b = 0 }, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
+            screen:fill({r = 0, g = 0, b = 0, a = 20}, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
             screen:copyfrom(self.image, nil, { x = screen:get_width()*(0.538 + (self.pos-9)*0.055), y = screen:get_height()*0.844, w=screen:get_width()*0.038, h = screen:get_height()*0.066 }, true)
         end
         self.image:destroy()
         collectgarbage()
     else
         if self.pos <= 8 then
-            screen:clear({r = 0, g = 0, b = 0}, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
+            screen:fill({r = 0, g = 0, b = 0, a = 20}, { x = screen:get_width()*(0.538 + (self.pos-1)*0.055), y = screen:get_height()*0.7435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
         else
-            screen:clear({r = 0, g = 0, b = 0}, { x = screen:get_width()*(0.538 + (self.pos-9)*0.055), y = screen:get_height()*0.8435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
+            screen:fill({r = 0, g = 0, b = 0, a = 20}, { x = screen:get_width()*(0.538 + (self.pos-9)*0.055), y = screen:get_height()*0.8435, w = screen:get_width()*0.039, h = screen:get_height()*0.068 })
         end
     end
     if prevPos ~= nil then
