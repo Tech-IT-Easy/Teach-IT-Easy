22f9c890572dfa16faf992ac97cfd69db9ba216c
Erik Jansson <erija860@student.liu.se>
2015-11-19 09:12:15 +0100

    Fixed bug in createProfile

--- Project/src/menus/CreateProfile.lua
+++ Project/src/menus/CreateProfile.lua
@@ -38,19 +38,21 @@ function CreateProfile:handleinput(event)
     self.pos = self.pos + 10
   elseif event.key == Event.KEY_UP and self.pos >= 11 then
     self.pos = self.pos - 10
---  elseif event.key == Event.KEY_ONE and self.pos < 27 then
---    self.profilename = self.profilename .. self.letters[self.pos] self:updatetext()
-  elseif event.key == Event.KEY_ONE and self.pos == 27 then
-    return { "chooseavatar" }
+    --  elseif event.key == Event.KEY_ONE and self.pos < 27 then
+    --    self.profilename = self.profilename .. self.letters[self.pos] self:updatetext()
+    --elseif event.key == Event.KEY_ONE and self.pos == 27 then
+    --  return { "chooseavatar" }
   elseif event.key == Event.KEY_OK and self.pos < 27 then
     if string.len(self.profilename) ~= 10 then
-    self.profilename = self.profilename .. self.letters[self.pos] self:updatetext()
+      self.profilename = self.profilename .. self.letters[self.pos] self:updatetext()
     end
   elseif event.key == Event.KEY_OK and self.pos == 27 then
-    return { "chooseavatar" }
---  elseif event.key == Event.KEY_TWO then
---    return { "profilesel", " " }
-    elseif event.key == Event.KEY_OK and self.pos == 28 then
+    if string.len(self.profilename) ~= 0 then
+      return { "chooseavatar" }
+    end
+    --  elseif event.key == Event.KEY_TWO then
+    --    return { "profilesel", " " }
+  elseif event.key == Event.KEY_OK and self.pos == 28 then
     self.profilename = string.sub(self.profilename, 1, -2)
     self:updatetext()
   elseif event.key == Event.KEY_BACK then
