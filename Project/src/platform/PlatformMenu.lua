e029ae3a3041c4a00b84011ecc34171d34891430
Erik Jansson <erija860@student.liu.se>
2015-11-26 16:50:24 +0100

    Updated tests for map.

--- Project/src/platform/PlatformMenu.lua
+++ Project/src/platform/PlatformMenu.lua
@@ -2,8 +2,8 @@
 -- This class is for the platform menu which is used
 -- to show game choice menu and process some key events
 --
--- @Author:Created by Chuck, Aug 16,2015
--- @Author:Updated by Tobias, Nov 02, 2015
+-- @Author : C r e a t e d by Chuck, Aug 16,2015
+-- @Author : U p d a t e d by Tobias, Nov 02, 2015
 -----------------------------------------------------------
 
 local EventHandler = require('toolkit.EventHandler')
@@ -28,12 +28,12 @@ local TrophyRoom = require('menus.TrophyRoom')
 -- @author Chuck
 -----------------------------------------------------------
 function PlatformMenu:new()
-  local o = PlatformMenu:super()
-  -- example code 
- -- self.position = {x=1,y=2 }
-  self:loadviews()
+    local o = PlatformMenu:super()
+    -- example code
+    -- self.position = {x=1,y=2 }
+    self:loadviews()
 
-  return PlatformMenu:init(o)
+    return PlatformMenu:init(o)
 end
 
 -----------------------------------------------------------
@@ -41,16 +41,16 @@ end
 -- @author Chuck
 -----------------------------------------------------------
 function PlatformMenu:show()
-  -- example code
-  screen:copyfrom(background, nil, { x = 0, y = 0, w = screen:get_width(), h = screen:get_height() }, true)
-  self.currentview:loadview()
-  --screen:clear({g=15, r=15, b=220}, {x=self.position.x*100,y=self.position.y*100,width=100,height=100})
+    -- example code
+    screen:copyfrom(background, nil, { x = 0, y = 0, w = screen:get_width(), h = screen:get_height() }, true)
+    self.currentview:loadview()
+    --screen:clear({g=15, r=15, b=220}, {x=self.position.x*100,y=self.position.y*100,width=100,height=100})
 end
 
 function PlatformMenu:update()
-  -- example code
-  self.currentview:update()
-  --screen:clear({g=15, r=15, b=220}, {x=self.position.x*100,y=self.position.y*100,width=100,height=100})
+    -- example code
+    self.currentview:update()
+    --screen:clear({g=15, r=15, b=220}, {x=self.position.x*100,y=self.position.y*100,width=100,height=100})
 end
 
 -------------------------------------
@@ -58,7 +58,7 @@ end
 -- @author Erik/ Marcus
 -------------------------------------
 function PlatformMenu:loadviews()
-    -- @member currentview:ProfileSelection
+    -- @member currentview:MenuSuperClass
     self.currentview = ProfileSelection:new()
     self.views = {
         profilesel = ProfileSelection,
@@ -77,7 +77,6 @@ end
 -- @author Erik/ Marcus
 -------------------------------------
 function PlatformMenu:changeview(newview)
-    -- @member currentview:MenuSuperClass
     self.currentview = self.views[newview[1]]:new()
     screen:copyfrom(background, nil, { x = 0, y = 0, w = screen:get_width(), h = screen:get_height() }, true)
     self.currentview:loadview(newview[2])
@@ -88,7 +87,7 @@ end
 -- Menu event handler for some keyboard input, when creating 
 -- a handler, you need to do a few the steps as follow
 -- @param object:PlatformMenu which will be operated, usually it is
---          the class that contain this handler, here is menu
+-- the class that contain this handler, here is menu
 -- @param event:Event which is event currently happening
 -----------------------------------------------------------
 
@@ -96,21 +95,20 @@ end
 menuEventHandler = EventHandler:new()
 
 -- register interesting events, listener will only notify these events
-menuEventHandler.events = {[Event.KEY_UP] = Event.KEY_STATE_DOWN,[Event.KEY_DOWN] = Event.KEY_STATE_DOWN,[Event.KEY_RIGHT]=Event.KEY_STATE_DOWN,[Event.KEY_LEFT]=Event.KEY_STATE_DOWN,[Event.KEY_ONE]=Event.KEY_STATE_DOWN,[Event.KEY_TWO]=Event.KEY_STATE_DOWN, [Event.KEY_OK]=Event.KEY_STATE_DOWN, [Event.KEY_BACK]=Event.KEY_STATE_DOWN}
+menuEventHandler.events = { [Event.KEY_UP] = Event.KEY_STATE_DOWN, [Event.KEY_DOWN] = Event.KEY_STATE_DOWN, [Event.KEY_RIGHT] = Event.KEY_STATE_DOWN, [Event.KEY_LEFT] = Event.KEY_STATE_DOWN, [Event.KEY_ONE] = Event.KEY_STATE_DOWN, [Event.KEY_TWO] = Event.KEY_STATE_DOWN, [Event.KEY_OK] = Event.KEY_STATE_DOWN, [Event.KEY_BACK] = Event.KEY_STATE_DOWN }
 
 -- override specific update method to response keyboard events, 
-function menuEventHandler:update(object,eventListener,event)
-  print("platform menu event handler")
+function menuEventHandler:update(object, eventListener, event)
+    print("platform menu event handler")
 
-   -- if key == 'exit' then
-   -- sys.stop()
-  --end
-  if event.state == Event.KEY_STATE_DOWN then
-    local temp = object.currentview:handleinput(event)
-    if temp[1] ~= " " then object:changeview(temp) end
-  collectgarbage()
-end 
- 
+    -- if key == 'exit' then
+    -- sys.stop()
+    --end
+    if event.state == Event.KEY_STATE_DOWN then
+        local temp = object.currentview:handleinput(event)
+        if temp[1] ~= " " then object:changeview(temp) end
+        collectgarbage()
+    end
 end
 
 -- Make handler useful to PlatformMenu
