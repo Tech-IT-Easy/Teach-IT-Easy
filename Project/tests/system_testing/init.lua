1bd066fc028c61a4431daf5d17c99f1926670230
jonathan <jonan099@student.liu.se>
2015-11-26 11:54:42 +0100

    Added a system test

--- Project/tests/system_testing/init.lua
+++ Project/tests/system_testing/init.lua
@@ -2,7 +2,8 @@
 -- Created by IntelliJ IDEA.
 -- User: jonathan
 -- Date: 2015-11-26
--- Time: 10:55
+-- Time: 11:20
 -- To change this template use File | Settings | File Templates.
 --
 
+require "tests.system_testing.SystemTest"
