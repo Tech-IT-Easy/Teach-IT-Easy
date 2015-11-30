2e89457d8dccaa231367b4992965a4f5e5f1c85f
Trygg <aletr899@student.liu.se>
2015-11-30 01:01:51 +0100

    Changed random stuff

--- Project/src/platform/PlatformContext.lua
+++ Project/src/platform/PlatformContext.lua
@@ -150,13 +150,12 @@ function PlatformContext:getProggLevels()
 --    levelMapData = "9acfffff503cffff5ff7ffff5fffffff7fffffff"
 --    levelGoalPosition = 20
 --    levelStartPosition = 33
-    level1 = {level = 1, maxCommands = {["queue"] = 13, ["loop"] = 11, ["P1"] = 13, ["P2"] = 16, ["if-wall"] = 16, ["if-not-wall"] = 16 }, mapData = "9acfffff503cffff5ff7ffff5fffffff7fffffff", levelGoalPosition = 20, levelStartPosition = 33, objectives = { 17, 1 } }
---      levelMapData = "3acad6ff503cffff5ff7ffff5fbc6fff7fffffff"
---      levelGoalPosition = 20
---      levelStartPosition = 33
-    level2 = {level = 1, maxCommands = {["queue"] = 8, ["loop"] = 6, ["P1"] = 12, ["P2"] = 5, ["if-wall"] = 5, ["if-not-wall"] = 5 }, mapData = "3acad6ff503cffff5ff7ffff5fbc6fff7fffffff", levelGoalPosition = 20, levelStartPosition = 33, objectives = {} }
+    level1 = {level = 1, maxCommands = {["queue"] = 13, ["loop"] = 11, ["P1"] = 13, ["P2"] = 16, ["if-wall"] = 16, ["if-not-wall"] = 16 }, mapData = "9acfffff503cffff5ff7ffff5fffffff7fffffff", levelGoalPosition = 20, levelStartPosition = 33 }
+--    levelMapData = "3acad6ff503cffff5ff7ffff5fbc6fff7fffffff"
+levelGoalPosition = 20
+levelStartPosition = 33
+    level2 = {level = 1, maxCommands = {["queue"] = 13, ["loop"] = 11, ["P1"] = 13, ["P2"] = 16, ["if-wall"] = 16, ["if-not-wall"] = 16 }, mapData = "9acfffff503cffff5ff7ffff5fffffff7fffffff", levelGoalPosition = 20, levelStartPosition = 33 }
     table.insert(levels, level1)
-    table.insert(levels, level2)
     return levels
 end
 
