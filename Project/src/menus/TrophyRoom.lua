bc6dc12e5f4c3a228a5ff57600b68e4e35392bc4
Erik Jansson <erija860@student.liu.se>
2015-11-30 01:40:27 +0100

    Added boxes in trophyRoom

--- Project/src/menus/TrophyRoom.lua
+++ Project/src/menus/TrophyRoom.lua
@@ -10,18 +10,12 @@ local Super = require('toolkit.MenuSuperClass')
 TrophyRoom = extends(Super)
 
 local Event = require('toolkit.Event')
---added to test game
 
 -------------------------------------
 -- Creates the TrophyRoom menu.
 -- @return self The created menu-object.
 -- @author Erik
 -------------------------------------
---[[function TrophyRoom:new()
-    o.totalTrophies = 5
-    return self
-end
-]]
 function TrophyRoom:new()
     local o = TrophyRoom:super()
     o.totalTrophies = 5
@@ -82,9 +76,12 @@ end
 -- @author Erik
 -------------------------------------
 function TrophyRoom:loadview(input)
-    --[[
+
     self.pos = 0
     self.lastpos = self.pos
+    self.activeGame= "progg"
+    self.games={progg= "platformContext.profile.gameprogress.progress['games.Progg.ProggGame']"}
+     --[[
     self.sidebuttons = { "Trophy room", "Wardrobe", "Settings" }
 
     --self:printbackground()
@@ -101,8 +98,12 @@ end
 function TrophyRoom:renderui()
     self:printTopPanel()
     self:printProgressionBar()
+    self:loadProgress()
 
+    self:loadButtons()
+    self:printAchievementBox()
 
+   --screen:clear({ g = 230, r = 230, b = 230 }, { x = screen:get_width() * 0.803, y = screen:get_height() * 0.0845, w = screen:get_width() * 0.0455, h = screen:get_height() * 0.0308 })
 
 
 
@@ -121,6 +122,69 @@ function TrophyRoom:renderui()
     ]]
 end
 
+function TrophyRoom:printAchievementBox()
+    screen:clear({ g = 131, r = 0, b = 143 }, { x = screen:get_width() * 0.57, y = self.starty, w = screen:get_width() * 0.35, h = screen:get_height() * 0.40 })
+     screen:clear({ g = 255, r = 255, b = 255 }, { x = screen:get_width() * 0.57, y = self.starty, w = screen:get_width() * 0.35, h = screen:get_height() * 0.40 })
+
+end
+
+-------------------------------------
+-- Loads the progress.
+-- @param game. The game to be showed.
+-- @author Erik
+-------------------------------------
+function TrophyRoom:loadProgress()
+    self.achievmentButtons = {}
+end
+
+-------------------------------------
+-- Loads information about the buttons.
+-- @author Erik
+-------------------------------------
+function TrophyRoom:loadButtons()
+    self.boxheight = screen:get_height() * 0.1
+    self.boxwidth = screen:get_width() * 0.3
+    -- local padding = (screen:get_width() * 0.75) - (self.boxheight * self.columns)
+    -- self.startx = padding / 2
+    self.starty = screen:get_height() * 0.45
+    self.innerboxheight = self.boxheight - 12
+    self.innerboxwidth= self.boxwidth- 12
+    self.boxpadding = 6
+
+
+    self:sidebuttonactive(1)
+    for i = 2, 3, 1 do
+        self:sidebuttoninactive(i)
+    end
+end
+
+-------------------------------------
+-- Prints an active button.
+-- @param x1. Which place to print button.
+-- @author Erik
+-------------------------------------
+function TrophyRoom:sidebuttonactive(x1)
+    screen:clear({ g = 131, r = 0, b = 143 }, { x = screen:get_width() * 0.08, y = self.starty + ((x1 - 1) * self.boxheight * 1.5), w = self.boxwidth, h = self.boxheight })
+    screen:clear({ g = 255, r = 255, b = 255 }, { x = screen:get_width() * 0.08+self.boxpadding, y = (self.starty + ((x1 - 1) * self.boxheight * 1.5))+self.boxpadding, w = self.innerboxwidth, h = self.innerboxheight })
+
+    --self.sidebuttonfonts[x1]:draw_over_surface(screen, self.sidebuttons[x1])
+end
+
+-------------------------------------
+-- Prints an inactive button.
+-- @param x1. Which place to print button.
+-- @author Erik
+-------------------------------------
+function TrophyRoom:sidebuttoninactive(x1)
+    screen:clear({ g = 228, r = 187, b = 235 }, { x = screen:get_width() * 0.08, y = self.starty + ((x1 - 1) * self.boxheight * 1.5), w = self.boxwidth, h = self.boxheight })
+
+    --self.sidebuttonfonts[x1]:draw_over_surface(screen, self.sidebuttons[x1])
+end
+
+-------------------------------------
+-- Prints the top panel.
+-- @author Erik
+-------------------------------------
 function TrophyRoom:printTopPanel()
     avatarImage = platformContext.profile.avatar
     local image1 = gfx.loadpng(platformContext.profile.avatar)
@@ -130,13 +194,17 @@ function TrophyRoom:printTopPanel()
     trophy_room_pagename:draw_over_surface(screen, "Programming game trophies")
 
     games_username:draw_over_surface(screen, self.usernamestring)
-    screen:copyfrom(image1, nil, { x = screen:get_width() * 0.08, y = screen:get_height() *0.09, w = screen:get_width() * 0.06, h = screen:get_height() * 0.1 })
+    screen:copyfrom(image1, nil, { x = screen:get_width() * 0.08, y = screen:get_height() * 0.09, w = screen:get_width() * 0.06, h = screen:get_height() * 0.1 })
     screen:clear({ g = 0, r = 0, b = 0 }, { x = screen:get_width() * 0.8, y = screen:get_height() * 0.08, w = screen:get_width() * 0.05, h = screen:get_height() * 0.04 })
     screen:clear({ g = 230, r = 230, b = 230 }, { x = screen:get_width() * 0.803, y = screen:get_height() * 0.0845, w = screen:get_width() * 0.0455, h = screen:get_height() * 0.0308 })
     games_backbutton:draw_over_surface(screen, "BACK")
     games_backtext:draw_over_surface(screen, "Go back")
 end
 
+-------------------------------------
+-- Prints the progression bar.
+-- @author Erik
+-------------------------------------
 function TrophyRoom:printProgressionBar()
 
     trophy_room_progress:draw_over_surface(screen, "You have completed " .. platformContext.profile.gameprogress.progress["games.Progg.ProggGame"].level .. " levels out of " .. self.totalTrophies)
