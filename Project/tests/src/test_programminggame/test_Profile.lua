9a4dccffc954f74bee2d4c19a845852805e6a8bf
Andreas Mansson <andma767@student.liu.se>
2015-12-01 20:11:21 +0100

    Updated test for Profile

--- Project/tests/src/test_programminggame/test_Profile.lua
+++ Project/tests/src/test_programminggame/test_Profile.lua
@@ -20,7 +20,7 @@ end
 function test_input_aName_picture1()
     local ps = require(SUT)
 
-    local a = ps:new("name", 'data/avatar/cute_robot/DOWN.png')
+    local a = ps:new("name", 'data/avatar/cute_robot/DOWN.png', 'data/avatar/cute_robot')
     local imagePath = a.avatar
     assert_equal(imagePath,'data/avatar/cute_robot/DOWN.png', "Should be data/avatar/cute_robot/DOWN.png")
 end
@@ -28,7 +28,7 @@ end
 function test_input_name_picture2()
     local ps = require(SUT)
 
-    local a = ps:new("name", 'data/avatar/insect_robot/DOWN.png')
+    local a = ps:new("name", 'data/avatar/insect_robot/DOWN.png', 'data/avatar/insect_robot')
     local imagePath = a.avatar
     assert_equal(imagePath,'data/avatar/insect_robot/DOWN.png', "Should be data/avatar/cute_robot/RIGHT.png")
 end
@@ -36,7 +36,7 @@ end
 function test_input_name_picture3()
     local ps = require(SUT)
 
-    local a = ps:new("name", 'data/avatar/cute_robot/UP.png')
+    local a = ps:new("name", 'data/avatar/cute_robot/UP.png', 'data/avatar/cute_robot')
     local imagePath = a.avatar
     assert_equal(imagePath, 'data/avatar/cute_robot/UP.png', "Should be data/avatar/cute_robot/RIGHT.png")
 end
