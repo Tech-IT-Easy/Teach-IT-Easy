4b2eea700ef561b57712fa492dd0b7edcaef8fe5
Andreas Mansson <andma767@student.liu.se>
2015-12-01 15:45:01 +0100

    Edited integration tests.

--- Project/tests/init.lua
+++ Project/tests/init.lua
@@ -2,7 +2,7 @@ require "luacov"
 require "tests.mock_emulator"
 
 -- Put your files after this line:
-require "tests.src"
+--require "tests.src"
 require "tests.integration_testing"
-require "tests.system_testing"
-require "tests.untested_files"
+--require "tests.system_testing"
+--require "tests.untested_files"
