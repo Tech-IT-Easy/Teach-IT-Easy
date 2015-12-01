21315b49866b46409f8d0222e6e04b3c57b6f431
Erik Jansson <erija860@student.liu.se>
2015-12-01 09:28:23 +0100

    Used new surface to reuse freetype object

--- Project/src/menus/TrophyRoom.lua
+++ Project/src/menus/TrophyRoom.lua
@@ -20,7 +20,7 @@ function TrophyRoom:new()
     local o = TrophyRoom:super()
     o.totalTrophies = 5
     o.sidebuttonfonts = { trophyroom_sidebuttontext1, trophyroom_sidebuttontext2, trophyroom_sidebuttontext3 }
-    o.inactivesidebuttonfonts = {trophyroom_inactive_sidebuttontext1, trophyroom_inactive_sidebuttontext2, trophyroom_inactive_sidebuttontext3}
+    o.inactivesidebuttonfonts = { trophyroom_inactive_sidebuttontext1, trophyroom_inactive_sidebuttontext2, trophyroom_inactive_sidebuttontext3 }
     return TrophyRoom:init(o)
 end
 
@@ -90,13 +90,13 @@ end
 -- @author Erik
 -------------------------------------
 function TrophyRoom:printAchievementBox()
-    screen:clear({ g = 131, r = 0, b = 143 }, { x = screen:get_width() * 0.57, y = self.starty, w = screen:get_width() * 0.35, h = screen:get_height() * 0.40 })
-    screen:clear({ g = 255, r = 255, b = 255 }, { x = (screen:get_width() * 0.57) + self.boxpadding, y = self.starty + self.boxpadding, w = (screen:get_width() * 0.35) - self.boxpadding * 2, h = (screen:get_height() * 0.40) - self.boxpadding * 2 })
+    screen:clear({ g = 131, r = 0, b = 143 }, { x = self.boxwidth+ self.boxmargin*2 , y = self.starty, w = self.boxwidth, h = screen:get_height() * 0.40 })
+    screen:clear({ g = 255, r = 255, b = 255 }, { x = self.boxwidth+ self.boxmargin*2 + self.boxpadding, y = self.starty + self.boxpadding, w = self.boxwidth - self.boxpadding * 2, h = (screen:get_height() * 0.40) - self.boxpadding * 2 })
     trophyroom_contentheader:draw_over_surface(screen, self.achievementButtons[self.pos][1])
     trophyroom_boxcontent:draw_over_surface(screen, self.achievementButtons[self.pos][2])
 
-    if(self.done[tostring(tostring(self.pos))]~=true)then
-    trophyroom_boxcontentrequirement:draw_over_surface(screen, self.achievementButtons[self.pos][3])
+    if (self.done[tostring(tostring(self.pos))] ~= true) then
+        trophyroom_boxcontentrequirement:draw_over_surface(screen, self.achievementButtons[self.pos][3])
     end
 end
 
@@ -111,19 +111,19 @@ function TrophyRoom:loadProgress(game)
     if (game == "progg") then
         if (platformContext.profile.gameprogress.progress["games.Progg.ProggGame"].proggGameLoopLevel) then
             table.insert(self.achievementButtons, { "Loop", "Loop is completed" })
-            self.done["1"]=true
+            self.done["1"] = true
         else
-            table.insert(self.achievementButtons, { "Loop", "Unlock loop levels","Finish level 2" })
+            table.insert(self.achievementButtons, { "Loop", "Unlock loop levels", "Finish level 2" })
         end
         if (platformContext.profile.gameprogress.progress["games.Progg.ProggGame"].proggGameProcLevel) then
             table.insert(self.achievementButtons, { "Procedure", "Procedure is completed" })
-            self.done["2"]=true
+            self.done["2"] = true
         else
             table.insert(self.achievementButtons, { "Procedure", "Unlock proceure levels", "Finish level 2 and 3" })
         end
         if (platformContext.profile.gameprogress.progress["games.Progg.ProggGame"].proggGameIfLevel) then
             table.insert(self.achievementButtons, { "If statement", "If statement is completed" })
-            self.done["3"]=true
+            self.done["3"] = true
         else
             table.insert(self.achievementButtons, { "If statement", "Unlock if statement", "Finish level 4 and 5" })
         end
@@ -137,7 +137,9 @@ end
 -------------------------------------
 function TrophyRoom:loadButtons()
     self.boxheight = screen:get_height() * 0.1
-    self.boxwidth = screen:get_width() * 0.3
+    self.boxwidth = screen:get_width() * 0.38
+    local freespace = screen:get_width()-(self.boxwidth*2)
+    self.boxmargin = freespace/3
     self.starty = screen:get_height() * 0.45
     self.innerboxheight = self.boxheight - 10
     self.innerboxwidth = self.boxwidth - 10
@@ -159,13 +161,20 @@ function TrophyRoom:sidebuttonactive(x1)
     screen:clear({ g = 255, r = 255, b = 255 }, { x = screen:get_width() * 0.08 + self.boxpadding, y = (self.starty + ((x1 - 1) * self.boxheight * 1.5)) + self.boxpadding, w = self.innerboxwidth, h = self.innerboxheight })
 
     --get info from tables
-    if(self.done[tostring(x1)])then
-    self.sidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
+    local text = gfx.new_surface(self.boxwidth, self.boxheight)
+    if (self.done[tostring(x1)]) then
+        trophyroom_sidebuttontext:draw_over_surface(text, self.achievementButtons[x1][1])
+        screen:copyfrom(text, nil, { x = screen:get_width() * 0.15 + self.boxpadding, y = (self.starty + ((x1 - 1) * self.boxheight * 1.5)) + self.boxpadding * 3, w = self.boxwidth, h = self.boxheight })
+
+        --self.sidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
     else
-    self.inactivesidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
-        --trophyroom_boxcontentrequirement
+        trophyroom_inactive_sidebuttontext:draw_over_surface(text, self.achievementButtons[x1][1])
+        screen:copyfrom(text, nil, { x = screen:get_width() * 0.15 + self.boxpadding, y = (self.starty + ((x1 - 1) * self.boxheight * 1.5)) + self.boxpadding * 3, w = self.boxwidth, h = self.boxheight })
+
+
+        --self.inactivesidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
     end
-    --self.sidebuttonfonts[x1]:draw_over_surface(screen, self.sidebuttons[x1])
+    text:destroy()
 end
 
 -------------------------------------
@@ -176,14 +185,22 @@ end
 function TrophyRoom:sidebuttoninactive(x1)
     screen:clear({ g = 228, r = 187, b = 235 }, { x = screen:get_width() * 0.08, y = self.starty + ((x1 - 1) * self.boxheight * 1.5), w = self.boxwidth, h = self.boxheight })
 
+    local text = gfx.new_surface(self.boxwidth, self.boxheight)
     --get info from tables
-    if(self.done[tostring(x1)])then
-    self.sidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
+    if (self.done[tostring(x1)]) then
+         trophyroom_sidebuttontext:draw_over_surface(text, self.achievementButtons[x1][1])
+        screen:copyfrom(text, nil, { x = screen:get_width() * 0.15 + self.boxpadding, y = (self.starty + ((x1 - 1) * self.boxheight * 1.5)) + self.boxpadding * 3, w = self.boxwidth, h = self.boxheight })
+
+
+        -- self.sidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
     else
-    self.inactivesidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
-        --trophyroom_boxcontentrequirement
+
+         trophyroom_inactive_sidebuttontext:draw_over_surface(text, self.achievementButtons[x1][1])
+        screen:copyfrom(text, nil, { x = screen:get_width() * 0.15 + self.boxpadding, y = (self.starty + ((x1 - 1) * self.boxheight * 1.5)) + self.boxpadding * 3, w = self.boxwidth, h = self.boxheight })
+
+        --self.inactivesidebuttonfonts[x1]:draw_over_surface(screen, self.achievementButtons[x1][1])
     end
-    --self.sidebuttonfonts[x1]:draw_over_surface(screen, self.sidebuttons[x1])
+    text:destroy()
 end
 
 -------------------------------------
--- Project/src/menus/skin.lua
+++ Project/src/menus/skin.lua
@@ -78,12 +78,17 @@ main_menu_sidebuttontext3 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 
 
 trophy_room_pagename = sys.new_freetype({ g = 255, r = 255, b = 255, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.28, y = main_menu_pagenamebaseline }, script_path .. 'data/Chalkduster.ttf')
 trophy_room_progress = sys.new_freetype({ g = 255, r = 255, b = 255, a = 255 }, screen:get_height() * 0.04, { x = screen:get_width() * 0.08, y = screen:get_height()*0.3 }, script_path .. 'data/GROBOLD.ttf')
-trophyroom_sidebuttontext1 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = screen:get_height() * 0.45+12 }, script_path .. 'data/condensed.ttf')
-trophyroom_sidebuttontext2 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = (screen:get_height() * 0.45 +  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
-trophyroom_sidebuttontext3 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y =(screen:get_height() * 0.45 + 2 *  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
-trophyroom_inactive_sidebuttontext1 = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = screen:get_height() * 0.45+12 }, script_path .. 'data/condensed.ttf')
-trophyroom_inactive_sidebuttontext2 = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = (screen:get_height() * 0.45 +  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
-trophyroom_inactive_sidebuttontext3 = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y =(screen:get_height() * 0.45 + 2 *  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
+
+trophyroom_sidebuttontext = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = 0, y = 0 }, script_path .. 'data/condensed.ttf')
+trophyroom_inactive_sidebuttontext = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = 0, y = 0 }, script_path .. 'data/condensed.ttf')
+
+
+--trophyroom_sidebuttontext1 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = screen:get_height() * 0.45+12 }, script_path .. 'data/condensed.ttf')
+--trophyroom_sidebuttontext2 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = (screen:get_height() * 0.45 +  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
+--trophyroom_sidebuttontext3 = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y =(screen:get_height() * 0.45 + 2 *  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
+--trophyroom_inactive_sidebuttontext1 = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = screen:get_height() * 0.45+12 }, script_path .. 'data/condensed.ttf')
+--trophyroom_inactive_sidebuttontext2 = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y = (screen:get_height() * 0.45 +  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
+--trophyroom_inactive_sidebuttontext3 = sys.new_freetype({ g = 178, r = 133, b = 201, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.15, y =(screen:get_height() * 0.45 + 2 *  screen:get_height() * 0.1 * 1.5) + 12 }, script_path .. 'data/condensed.ttf')
 
 trophyroom_contentheader = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.08, { x = screen:get_width() * 0.6, y = screen:get_height() * 0.45+12 }, script_path .. 'data/condensed.ttf')
 trophyroom_boxcontent = sys.new_freetype({ g = 131, r = 0, b = 143, a = 255 }, screen:get_height() * 0.05, { x = screen:get_width() * 0.6, y = screen:get_height() * 0.55+12 }, script_path .. 'data/condensed.ttf')
