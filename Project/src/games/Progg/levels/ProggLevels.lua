bc001f886bd8a72aa5c76a508d7a4657f740fdec
Trygg <aletr899@student.liu.se>
2015-12-02 10:05:06 +0100

    Added some levels

--- Project/src/games/Progg/levels/ProggLevels.lua
+++ Project/src/games/Progg/levels/ProggLevels.lua
@@ -21,10 +21,10 @@ end
 
 function ProggLevels:getProggLevels()
     local levels = {}
-    local level1 = {level = 1, maxCommands = {["queue"] = 16, ["loop"] = 0, ["P1"] = 0, ["P2"] = 0, ["if-wall"] = 0, ["if-not-wall"] = 0 }, mapData = "dfffffff5fffffff5fffffff5fffffff7fffffff", levelGoalPosition = 1, levelStartPosition = 33, objectives = {} }
-    local level2 = {level = 2, maxCommands = {["queue"] = 8, ["loop"] = 6, ["P1"] = 12, ["P2"] = 5, ["if-wall"] = 5, ["if-not-wall"] = 5 }, mapData = "ffffffffffffffff9aefffff5fffffff7fffffff", levelGoalPosition = 20, levelStartPosition = 33, objectives = {} }
-    local level3 = {level = 3, maxCommands = {["queue"] = 16, ["loop"] = 16, ["P1"] = 16, ["P2"] = 16, ["if-wall"] = 16, ["if-not-wall"] = 16 }, mapData = "0000000000000000000000000000000000000000", levelGoalPosition = 20, levelStartPosition = 33, objectives = {} }
-    local level4 = {level = 4, maxCommands = {["queue"] = 16, ["loop"] = 16, ["P1"] = 16, ["P2"] = 16, ["if-wall"] = 16, ["if-not-wall"] = 16 }, mapData = "0000000000000000000000000000000000000000", levelGoalPosition = 20, levelStartPosition = 33, objectives = {} }
+    local level1 = {level = 1, maxCommands = {["queue"] = 8, ["loop"] = 0, ["P1"] = 0, ["P2"] = 0, ["if-wall"] = 0, ["if-not-wall"] = 0 }, mapData = "dfffffff5fffffff5fffffff5fffffff7fffffff", levelGoalPosition = 1, levelStartPosition = 33, objectives = {} }
+    local level2 = {level = 2, maxCommands = {["queue"] = 8, ["loop"] = 0, ["P1"] = 0, ["P2"] = 0, ["if-wall"] = 0, ["if-not-wall"] = 0 }, mapData = "ffffffffffffffff9aefffff5fffffff7fffffff", levelGoalPosition = 19, levelStartPosition = 33, objectives = {} }
+    local level3 = {level = 3, maxCommands = {["queue"] = 16, ["loop"] = 0, ["P1"] = 0, ["P2"] = 0, ["if-wall"] = 0, ["if-not-wall"] = 0 }, mapData = "fffffffffffffffff9efffff96ffffff7fffffff", levelGoalPosition = 19, levelStartPosition = 33, objectives = {26} }
+    local level4 = {level = 4, maxCommands = {["queue"] = 16, ["loop"] = 16, ["P1"] = 16, ["P2"] = 16, ["if-wall"] = 16, ["if-not-wall"] = 16 }, mapData = "9aaaaacf59aaac5f55baa65f53aaaa6f7fffffff", levelGoalPosition = 19, levelStartPosition = 33, objectives = {} }
     table.insert(levels, level1)
     table.insert(levels, level2)
     table.insert(levels, level3)
