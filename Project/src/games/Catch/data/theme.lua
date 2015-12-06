69262fcff3c36bc1838089aa2ef9b1d340378cf1
Kai Chu <kaich284@student.liu.se>
2015-12-06 12:20:41 +0100

    Fixed to test on STB step by step

--- Project/src/games/Catch/data/theme.lua
+++ Project/src/games/Catch/data/theme.lua
@@ -2,7 +2,7 @@ local THEME = {}
 
 THEME.COLOR = {
   RED = {r=0,g=255,b=0,a=255 },
-  WHITE = {r=255,g=255,b=255,a=255 },
+  WHITE = {g=255, r=255, b=255, a=255},
   BLUE =  {r=0,g=0,b=255,a=255 },
   LIGHT_BLUE = {r=78,g=114,b=210,a=255 },
   GREEN = {r=0,g=255,b=0,a=255 },
--- Project/src/toolkit/UIKit/UILabelView.lua
+++ Project/src/toolkit/UIKit/UILabelView.lua
@@ -1,5 +1,5 @@
 -----------------------------------------------------------
--- This class is a UIView class for showing a Label in specific 
+-- This class is a UIView class for showing a Label in specific
 -- position. After label is created, it is constant so that it can't
 --  be changed. Although you change it, nothing will happen.
 --
@@ -9,25 +9,24 @@
 local UIView = require("toolkit.UIKit.UIView")
 local UILabelView = extends(UIView)
 
---@member static constant 
-
-
+--@member static constant
 UILabelView.FONT_GROBOLD = 'data/GROBOLD.ttf'
 UILabelView.FONT_CHALKDUSTER = 'data/Chalkduster.ttf'
-  
+
+local script_path = nil
+if ADConfig.isSimulator then
+  script_path = ""
+else
+  script_path = sys.root_path()
+end
+
 function UILabelView:new(args)
   --@member container which contains this LabelView,default nil which means whole window
   local o = UILabelView:super({frame=args.frame,container=args.container})
-  --@member label UILabel type 
+  --@member label UILabel type
   o.label = args.label
-  --@member label data 
-  
-  local script_path = nil
-  if ADConfig.isSimulator then
-    script_path = ""
-  else
-    script_path = sys.root_path()
-  end
+  --@member label data
+
   -- o.labelData = sys.new_freetype(o.label.color, o.label.size, {x = o.globalFrame.x,y = o.globalFrame.y},o.label.font)
   o.labelData = sys.new_freetype({g=255, r=255, b=255, a=255}, 70, {x= 20, y=20}, script_path..'data/GROBOLD.ttf')
 
@@ -39,8 +38,8 @@ function UILabelView:show()
 end
 
 function UILabelView:afterUpdateGlobalFrame()
-  self.labelData = sys.new_freetype({g=255, r=255, b=255, a=255}, 70, {x= 20, y=20}, script_path..'data/GROBOLD.ttf')
-  --self.labelData = sys.new_freetype(self.label.color, self.label.size,{x = self.globalFrame.x,y = self.globalFrame.y},self.label.font)
+  -- self.labelData = sys.new_freetype({g=255, r=255, b=255, a=255}, 70, {x= 20, y=20}, script_path..'data/GROBOLD.ttf')
+  self.labelData = sys.new_freetype(self.label.color, self.label.size,{x = self.globalFrame.x,y = self.globalFrame.y},script_path..self.label.font)
 end
 
 return UILabelView
